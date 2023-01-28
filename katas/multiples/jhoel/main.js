c139ed09d87ff9661afee2ca9a783c6798e79a25
Jhoel Flores <Jhoel.Flores@jala.univesity>
2023-01-28 14:16:53 +0100

    done the multiples katas task

---
+++ katas/multiples/jhoel/main.js
@@ -0,0 +1,21 @@
+const numberLimit = process.argv[2] // first argument
+
+if (!Number.isInteger(Number(numberLimit)))
+    throw new Error('The argument must be an integer')
+
+
+const multiples = (numberLimit) => {
+
+    let sum = 0
+
+    for (let number = 1; number < numberLimit; number++) {
+        if (number % 3 === 0 || number % 5 === 0) {
+            sum += number
+        }
+    }
+
+    return sum
+
+}
+
+console.log(`Sum of multiples of 3 and 5 below ${numberLimit} is\x1b[32m ${multiples(numberLimit)}\x1b[37m`)
