87a59fb43f2b04f96c06e98f8afc93dc8df06cd8
Bryan Colque Clavijo <Bryan.Colque@jala.university>
2023-01-29 14:40:55 -0400

    Katas folder added with Bryan's folders and files

---
+++ katas/multiples/bryan/multiples_bryan_colque.js
@@ -0,0 +1,32 @@
+var top;
+var count; 
+var sum=0; 
+var numb;
+
+
+
+function multiples(top){
+
+if(top<0){
+    return 0;
+}
+
+else{
+for(count=0;count<top;count++) {
+    if(count%5==0 || count%3==0){
+            sum+=count;
+    }
+}
+
+return sum;
+}
+    }
+ 
+
+
+numb=20;
+console.log(multiples(numb));
+
+
+
+
