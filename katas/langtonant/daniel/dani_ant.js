177f3883c0e963d2e1bda33af85c4f923144523b
Daniel Carrasco <Daniel.Carrasco@jala.university>
2023-01-30 12:26:46 -0400

    fixed errors in code solved

--- katas/langtonant/daniel/dani_ant.js
+++ katas/langtonant/daniel/dani_ant.js
@@ -1,4 +1,4 @@
-// create a grid
+
 let grid = [];
 for (let i = 0; i < 100; i++) {
     let row = [];
@@ -7,52 +7,43 @@ for (let i = 0; i < 100; i++) {
     }
     grid.push(row);
 }
-// initial pos for the ant
 let antRow = 50;
 let antCol = 50;
-
-// ant direction
-let antDirection = "up";
-
-// steps for the ant
+let antDirection = 'up';
 for (let step = 0; step < 10000; step++) {
-    // color cell
     let currentColor = grid[antRow][antCol];
-    
-    // change actual color of cell
     grid[antRow][antCol] = currentColor === 0 ? 1 : 0;
-    
-    // ant turn
+
     if (currentColor === 0) {
-        if (antDirection === "up") {
-            antDirection = "right";
-        } else if (antDirection === "right") {
-            antDirection = "down";
-        } else if (antDirection === "down") {
-            antDirection = "left";
-        } else if (antDirection === "left") {
-            antDirection = "up";
+        if (antDirection === 'up') {
+            antDirection = 'right';
+        } else if (antDirection === 'right') {
+            antDirection = 'down';
+        } else if (antDirection === 'down') {
+            antDirection = 'left';
+        } else if (antDirection === 'left') {
+            antDirection = 'up';
         }
     } else {
-        if (antDirection === "up") {
-            antDirection = "left";
-        } else if (antDirection === "left") {
-            antDirection = "down";
-        } else if (antDirection === "down") {
-            antDirection = "right";
-        } else if (antDirection === "right") {
-            antDirection = "up";
+        if (antDirection === 'up') {
+            antDirection = 'left';
+        } else if (antDirection === 'left') {
+            antDirection = 'down';
+        } else if (antDirection === 'down') {
+            antDirection = 'right';
+        } else if (antDirection === 'right') {
+            antDirection = 'up';
         }
     }
-    
-    // ant movement
-    if (antDirection === "up") {
+
+
+    if (antDirection === 'up') {
         antRow--;
-    } else if (antDirection === "right") {
+    } else if (antDirection === 'right') {
         antCol++;
-    } else if (antDirection === "down") {
+    } else if (antDirection === 'down') {
         antRow++;
-    } else if (antDirection === "left") {
+    } else if (antDirection === 'left') {
         antCol--;
     }
 }
