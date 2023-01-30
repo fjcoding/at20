f7dd5fee7420a9efd1daa3462e2dc9898941c2de
Maria Huampo <Maria.Huampo@jala.university>
2023-01-30 09:21:25 -0400

    Fixed some syntaxi errors to katas/maria folder

--- katas/langtonant/maria/langtonant.js
+++ katas/langtonant/maria/langtonant.js
@@ -2,10 +2,10 @@ var gridSize = process.argv[2];
 var steps = process.argv[3];
 
 var gridRow = new Array();
-for (let x = 0; x < gridSize; x++){
+for (let x = 0; x < gridSize; x++) {
     gridRow[x] = new Array();
-    for (let y = 0; y < gridSize; y++){
-        gridRow[x][y]= 0;
+    for (let y = 0; y < gridSize; y++) {
+        gridRow[x][y] = 0;
     }
 }
 
@@ -13,72 +13,63 @@ var direction = 'up';
 var positionX = Math.trunc(gridSize / 2);
 var positionY = Math.trunc(gridSize / 2);
 var color = 0;
-while (steps > 0){
-    if (positionX >= 0 && positionY >= 0){
-        if (direction == 'up'){
-            if (color == 0){
+while (steps > 0) {
+    if (positionX >= 0 && positionY >= 0) {
+        if (direction == 'up') {
+            if (color == 0) {
                 gridRow[positionX][positionY] = 1;
                 positionY = positionY + 1;
                 color = gridRow[positionX][positionY];
                 direction = 'right';
-            }       
-            else if (color == 1){
-                gridRow[positionX][positionY] =0;
+            } else if (color == 1) {
+                gridRow[positionX][positionY] = 0;
                 positionY = positionY - 1;
                 color = gridRow[positionX][positionY];
                 direction = 'left';
-            } 
-        }
-        else if (direction == 'right'){
-            if (color == 0){
-                gridRow[positionX][positionY] =1;
+            }
+        } else if (direction == 'right') {
+            if (color == 0) {
+                gridRow[positionX][positionY] = 1;
                 positionX = positionX + 1;
                 color = gridRow[positionX][positionY];
                 direction = 'down';
-            }
-            else if (color == 1){
-                gridRow[positionX][positionY] =0;
+            } else if (color == 1) {
+                gridRow[positionX][positionY] = 0;
                 positionX = positionX - 1;
                 color = gridRow[positionX][positionY];
                 direction = 'up';
-            } 
-        }
-        else if (direction == 'down'){
-            if (color == 0){
+            }
+        } else if (direction == 'down') {
+            if (color == 0) {
                 gridRow[positionX][positionY] = 1;
                 positionY = positionY - 1;
                 color = gridRow[positionX][positionY];
                 direction = 'left';
-            }
-            else if (color == 1){
-                gridRow[positionX][positionY] =0;
+            } else if (color == 1) {
+                gridRow[positionX][positionY] = 0;
                 positionY = positionY + 1;
                 color = gridRow[positionX][positionY];
                 direction = 'right';
-            } 
-        }
-        else if (direction == 'left'){
-            if (color == 0){
-                gridRow[positionX][positionY] =1;
+            }
+        } else if (direction == 'left') {
+            if (color == 0) {
+                gridRow[positionX][positionY] = 1;
                 positionX = positionX - 1;
                 color = gridRow[positionX][positionY];
                 direction = 'up';
-            }
-            else if (color == 1){
+            } else if (color == 1) {
                 gridRow[positionX][positionY] = 0;
                 positionX = positionX + 1;
                 color = gridRow[positionX][positionY];
                 direction = 'down';
-            } 
-        }   
-        steps--
-    }
-    else {
+            }
+        }
+        steps--;
+    } else {
         steps = 0;
         console.log('off the grid');
         //console.log(gridRow);
-
-    }  
-//console.log(gridRow) 
+    }
+//console.log(gridRow)
 }
 console.log(gridRow);
