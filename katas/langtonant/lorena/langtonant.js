eb9ba169e51caed6612a37d4f6d89953192ce24a
Lorena Ramirez <Lorena.Ramirez@jala.university>
2023-01-30 11:10:37 -0500

    Fix files errors in lorena/katas

--- katas/langtonant/lorena/langtonant.js
+++ katas/langtonant/lorena/langtonant.js
@@ -1,26 +1,22 @@
 //lANGTON'S ANT//
 
-//Funtion: Ant´s displacement 
+//Funtion: Ant´s displacement
 
 function mov(x, y, ori) {
     var movant = new Array();
-    if(ori == 0 && y != 0){
+    if (ori == 0 && y != 0) {
         y = y - 1;
-    }
-    else if (ori == 1 && x != (wid-1)){
+    } else if (ori == 1 && x != (wid - 1)) {
         x = x + 1;
-    }
-    else if (ori == 2 && y != (len-1)){        
+    } else if (ori == 2 && y != (len - 1)) {
         y = y + 1;
-    }
-    else if (ori == 3 && x != 0){
+    } else if (ori == 3 && x != 0) {
         x = x - 1;
+    } else {
+        ori = (ori + 2) % 4;           //If the ant is on the limit, turn 180° ???
     }
-    else {
-        ori=(ori + 2) %4;           //If the ant is on the limit, turn 180° ???
-    }
-    movant = new Array (x,y,ori)
-    
+    movant = new Array (x, y, ori);
+
     return movant;
 }
 
@@ -30,43 +26,41 @@ var grid = new Array();
 var len = 6;
 var wid = 6;
 
-for (i=0; i<len; i++) {
-    grid[i]=new Array();
-    for(j=0; j<wid; j++) {       
-        grid[i][j]=0;        
+for (let i = 0; i < len; i++) {
+    grid[i] = new Array();
+    for (let j = 0; j < wid; j++) {
+        grid[i][j] = 0;
     }
-    console.log(grid[i])
+    console.log(grid[i]);
 }
 
 // Initial Ant´s position
 
 var x = Math.floor(Math.random() * ((wid) - 0) + 0);     // x position random
-var y=Math.floor(Math.random() * ((len) - 0) + 0);      // y position random
-var ori=Math.floor(Math.random() * ((4) - 0) + 0);      // Ant´s orientation Up=0, Right=1, Down=2, Left=3
+var y = Math.floor(Math.random() * ((len) - 0) + 0);      // y position random
+var ori = Math.floor(Math.random() * ((4) - 0) + 0);      // Ant´s orientation Up=0, Right=1, Down=2, Left=3
 
 var movant = new Array(x, y, ori); // firest box position
 
-console.log(movant)
+console.log(movant);
 
 
-var n=10;                        // Tracking number
+var n = 10;                        // Tracking number
 
-while(n > 0 ){
-
-    if (grid[y][x] == 0){           // Ant´s Orientation
+while (n > 0) {
+    if (grid[y][x] == 0) {           // Ant´s Orientation
         ori = (ori + 1) % 4;
-        grid[y][x]= 1
-    }
-    else{
+        grid[y][x] = 1;
+    } else {
         ori = (ori + 3) % 4;
-        grid[y][x] = 0   
+        grid[y][x] = 0;
     }
 
-    movant = mov(x,y,ori)
+    movant = mov(x, y, ori);
     x = movant[0];
     y = movant[1];
 
-    console.log (grid)
-    console.log(movant)
+    console.log (grid);
+    console.log(movant);
     n--;
 }
