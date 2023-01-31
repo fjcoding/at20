33b2ef3873d2a39efdd6e85de1e4cd39c8dab0a1
Giovanni Blanco <Giovanni.Blanco@jala.university>
2023-01-31 08:00:14 -0400

    Add corrections about code conventions

--- katas/langtonant/Giovanni/langtonsant.js
+++ katas/langtonant/Giovanni/langtonsant.js
@@ -3,6 +3,14 @@ let map = new Array(mapdim).fill().map(() => Array(mapdim).fill(0));
 let visualmap = map;
 const ant = {};
 
+const NORTH = 0;
+const EAST = 1;
+const SOUTH = 2;
+const WEST = 3;
+
+const BLACKcase = 1;
+const WHITEcase = 0;
+
 const LEFT = 3;
 const RIGHT = 1;
 
@@ -11,51 +19,38 @@ ant.x = mapdim / 2;
 ant.y = mapdim / 2;
 ant.dir = 0;
 
-function turn(nextdir) {
-    ant.dir = (ant.dir + nextdir) % 4;
-}
-
-function advance() {
-    switch (ant.dir) {
-    case 0:
-        ant.y--;
-        break;
-    case 2:
-        ant.y++;
-        break;
-    case 1:
-        ant.x++;
-        break;
-    case 3:
-        ant.x--;
-        break;
-    }
-}
-
-function actualizemap() {
-    visualmap = map;
-    visualmap[ant.y][ant.x] = ant.symbol;
-    console.log(visualmap);
-}
-
 //WHILE TRUE
-function step() {
-    let color = map[ant.y][ant.x];
-    //console.log(color);
-    switch (color) {
-    case 0:  //BLANCO
-        map[ant.y][ant.x] = 1;
-        turn(RIGHT);
-        break;
-    case 1:  //NEGRO
-        map[ant.y][ant.x] = 0;
-        turn(LEFT);
-        break;
-    }
-    advance();
+
+visualmap = map;
+visualmap[ant.y][ant.x] = ant.symbol;
+console.log(visualmap);
+
+let color = map[ant.y][ant.x];
+switch (color) {
+case WHITEcase:  //BLANCO
+    map[ant.y][ant.x] = 1;
+    ant.dir = (ant.dir + RIGHT) % 4;
+    break;
+case BLACKcase:  //NEGRO
+    map[ant.y][ant.x] = 0;
+    ant.dir = (ant.dir + LEFT) % 4;
+    break;
+}
+switch (ant.dir) {
+case NORTH:
+    ant.y--;
+    break;
+case SOUTH:
+    ant.y++;
+    break;
+case EAST:
+    ant.x++;
+    break;
+case WEST:
+    ant.x--;
+    break;
 }
 
-
-actualizemap();
-console.log(map);
-step();
+visualmap = map;
+visualmap[ant.y][ant.x] = ant.symbol;
+console.log(visualmap);
