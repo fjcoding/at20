28bfaec8294ffcd79279ca2126e290e64bf05f87
Giovanni Blanco <Giovanni.Blanco@jala.university>
2023-01-31 19:33:13 -0400

    Actualize langston ant code with testing file

--- katas/langtonant/Giovanni/langtonsant.js
+++ katas/langtonant/Giovanni/langtonsant.js
@@ -1,8 +1,8 @@
-const mapDim = 6;
+export const mapDim = 6;
 
 let map = new Array(mapDim).fill().map(() => Array(mapDim).fill(0));
 
-const ant = {};
+export const ant = {};
 
 const up = 0;
 const right = 1;
@@ -18,13 +18,9 @@ const turnRight = 1;
 ant.symbol = 8;
 ant.x = mapDim / 2;
 ant.y = mapDim / 2;
-ant.dir = 3;
+ant.dir = left;
 
-for (let iterations = 0; iterations < 10; iterations++) {
-    let color = map[ant.y][ant.x];
-    map[ant.y][ant.x] = ant.symbol;
-    console.log(map);
-
+export function turn(color) {
     switch (color) {
     case white:
         map[ant.y][ant.x] = 1;
@@ -37,7 +33,11 @@ for (let iterations = 0; iterations < 10; iterations++) {
         map[ant.y][ant.x] = white;
         break;
     }
+    return (ant.dir);
+}
 
+
+export function advance() {
     switch (ant.dir) {
     case up:
         ant.y--;
@@ -52,6 +52,17 @@ for (let iterations = 0; iterations < 10; iterations++) {
         ant.x--;
         break;
     }
+    return (ant.x, ant.y);
+}
+
+
+for (let iterations = 0; iterations < 10; iterations++) {
+    let color = map[ant.y][ant.x];
+    map[ant.y][ant.x] = ant.symbol;
+    console.log(map);
+
+    turn(color);
+    advance();
 }
 map[ant.y][ant.x] = ant.symbol;
 console.log(map);
---
+++ katas/langtonant/Giovanni/langtonsant.test.js
@@ -0,0 +1,38 @@
+import { turn } from './langtonsant';
+import { advance } from './langtonsant';
+import { ant } from './langtonsant';
+
+test('Function that turns the ant according to the color of its position', () => {
+    const black = 1;
+    const white = 0;
+    //         up = 0;
+    //left = 3;        right = 1;
+    //         down = 2;
+    ant.dir = 0;
+    expect(turn(black)).toBe(3);
+    ant.dir = 0;
+    expect(turn(white)).toBe(1);
+    ant.dir = 2;
+    expect(turn(black)).toBe(1);
+    ant.dir = 2;
+    expect(turn(white)).toBe(3);
+    ant.dir = 1;
+    expect(turn(black)).toBe(0);
+    ant.dir = 1;
+    expect(turn(white)).toBe(2);
+    ant.dir = 3;
+    expect(turn(black)).toBe(2);
+    ant.dir = 3;
+    expect(turn(white)).toBe(0);
+});
+
+//test('Function that advances de position according to the orientation', () => {
+//      --------x
+//      |
+//      |    *
+//      y
+//ant.dir = 0;
+//ant.x = 10;
+//ant.y = 10;
+//expect(advance()).arrayContaining([10, 9]);
+//});
