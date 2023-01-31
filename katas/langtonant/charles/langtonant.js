e4d4a2b85283b3e3e58a7c63e38f824833479234
Charles Flores <Charles.Flores@jala.university>
2023-01-31 02:32:12 -0400

    Updated code to solve Langton's ant exercise in langtonant.js file

--- katas/langtonant/charles/langtonant.js
+++ katas/langtonant/charles/langtonant.js
@@ -1,100 +1,82 @@
-// const dimension = parseInt(prompt('Enter the dimension of the grid: '));
-// const nSteps = parseInt(prompt('Enter number of steps: '));
-// const speed = parseInt(prompt('Enter speed (millisecond): '));
+const arg1 = process.argv[2];//N dimension grid
+const arg2 = process.argv[3];//N steps
 
-const dimension = 30;
-const nSteps = 100;
-const speed = 200;
+let gridArray = new Array(arg1);
+let rowPositionStep;
+let columnPositionStep;
+let pointer;
+let isPainted = false;
+let startPosition;
 
+const directionRight = '[r]';
+const directionLeft = '[l]';
+const directionUp = '[u]';
+const directionDown = '[d]';
 
-let initialPosition;
-let antPosition;
-let pointerCell;
-let directionPainted;
-var document;
+const startStepDirection = directionUp;
 
 function createGrid() {
-    let body = document.getElementsByTagName('body')[0];
-    let bodyGrid = document.createElement('table');
-    let tableBody = document.createElement('tbody');
-
-    let cellNumber = 1;
-    let cellMiddle = dimension % 2 == 0 ? (dimension * dimension) / 2 - (dimension / 2) + 1 : (dimension * dimension) / 2 + 0.5;
-
-    initialPosition = cellMiddle;
-    let cellPainted = 0;
-    let cellColor = 'white';
-
-    for (let i = 0; i < dimension; i++) {
-        let gridRow = document.createElement('tr');
-        for (let j = 0; j < dimension; j++) {
-            let cell = document.createElement('td');
-            let cellContent = document.createElement('div');
-            cellContent.style = 'padding: 10px; background: ' + cellColor;
-            cellContent.setAttribute('data-painted', cellPainted);
-            let cellDirection = cellMiddle == cellNumber ? 'r' : '';
-            cellContent.setAttribute('data-direction', cellDirection);
-            cellContent.id = 'c' + cellNumber;
-            cell.appendChild(cellContent);
-            gridRow.appendChild(cell);
-            cellNumber++;
+    startPosition = arg1 % 2 == 0 ? (arg1) / 2 - 1 : (arg1) / 2 - 0.5;
+    for (let indexRow = 0; indexRow < arg1; indexRow++) {
+        gridArray[indexRow] = new Array(arg1);
+        for (let indexColumn = 0; indexColumn < arg1; indexColumn++) {
+            gridArray[indexRow][indexColumn] = (indexRow == startPosition && indexColumn == startPosition) ? startStepDirection : '   ';
         }
-        tableBody.appendChild(gridRow);
     }
-
-    bodyGrid.appendChild(tableBody);
-    bodyGrid.setAttribute('border', '1');
-    body.appendChild(bodyGrid);
 }
 
-function langtonAntStart() {
-    antPosition = document.getElementById(`c${initialPosition}`);
-    pointerCell = initialPosition;
-    let steps = nSteps;
-
-    const timer = setInterval(() => {
-        let changeColor = 'white';
-        let paintCell = 0;
-        if (antPosition.getAttribute('data-painted') == 0) {
-            changeColor = 'black';
-            paintCell = 1;
-        }
-        antPosition.setAttribute('style', 'background: ' + changeColor + '; padding: 10px;');
-        antPosition.setAttribute('data-painted', paintCell);
-        let direction = antPosition.getAttribute('data-direction');
-        directionNextStep(direction);
-        steps--;
-        if (steps == 0) {
-            clearInterval(timer);
-        }
-    }, speed);
-}
-
-function directionNextStep(direction) {
-    switch (direction) {
-    case 'u':
-        pointerCell -= dimension;
-        directionPainted = document.getElementById(`c${pointerCell}`).getAttribute('data-painted') == 0 ? 'r' : 'l';
-        document.getElementById(`c${pointerCell}`).setAttribute('data-direction', directionPainted);
+function directionNextStep(rowPosition, columnPosition) {
+    switch (gridArray[rowPosition][columnPosition]) {
+    case directionUp:
+        pointer = rowPosition - 1;
+        gridArray[rowPosition][columnPosition] = !isPainted ? '[o]' : '   ';
+        isPainted = gridArray[pointer][columnPosition] == '   ' ? false : true;
+        gridArray[pointer][columnPosition] = gridArray[pointer][columnPosition] == '   ' ? directionRight : directionLeft;
+        rowPositionStep = pointer;
         break;
-    case 'd':
-        pointerCell += dimension;
-        directionPainted = document.getElementById(`c${pointerCell}`).getAttribute('data-painted') == 0 ? 'l' : 'r';
-        document.getElementById(`c${pointerCell}`).setAttribute('data-direction', directionPainted);
+    case directionDown:
+        pointer = rowPosition + 1;
+        gridArray[rowPosition][columnPosition] = !isPainted ? '[o]' : '   ';
+        isPainted = gridArray[pointer][columnPosition] == '   ' ? false : true;
+        gridArray[pointer][columnPosition] = gridArray[pointer][columnPosition] == '   ' ? directionLeft : directionRight;
+        rowPositionStep = pointer;
         break;
-    case 'r':
-        pointerCell++;
-        directionPainted = document.getElementById(`c${pointerCell}`).getAttribute('data-painted') == 0 ? 'd' : 'u';
-        document.getElementById(`c${pointerCell}`).setAttribute('data-direction', directionPainted);
+    case directionRight:
+        pointer = columnPosition + 1;
+        gridArray[rowPosition][columnPosition] = !isPainted ? '[o]' : '   ';
+        isPainted = gridArray[rowPosition][pointer] == '   ' ? false : true;
+        gridArray[rowPosition][pointer] = gridArray[rowPosition][pointer] == '   ' ? directionDown : directionUp;
+        columnPositionStep = pointer;
         break;
-    case 'l':
-        pointerCell--;
-        directionPainted = document.getElementById(`c${pointerCell}`).getAttribute('data-painted') == 0 ? 'u' : 'd';
-        document.getElementById(`c${pointerCell}`).setAttribute('data-direction', directionPainted);
+    case directionLeft:
+        pointer = columnPosition - 1;
+        gridArray[rowPosition][columnPosition] = !isPainted ? '[o]' : '   ';
+        isPainted = gridArray[rowPosition][pointer] == '   ' ? false : true;
+        gridArray[rowPosition][pointer] = gridArray[rowPosition][pointer] == '   ' ? directionUp : directionDown;
+        columnPositionStep = pointer;
         break;
     }
-    antPosition = document.getElementById(`c${pointerCell}`);
 }
 
-createGrid();
-langtonAntStart();
+function showGrid() {
+    for (let indexRow = 0; indexRow < arg1; indexRow++) {
+        let rowContent = '';
+        for (let indexColumn = 0; indexColumn < arg1; indexColumn++) {
+            rowContent += gridArray[indexRow][indexColumn] + '';
+        }
+        console.log(rowContent);
+    }
+}
+
+function main() {
+    createGrid();
+    const steps = arg2;
+    rowPositionStep = startPosition;
+    columnPositionStep = startPosition;
+    for (let index = 0; index < steps; index++) {
+        directionNextStep(rowPositionStep, columnPositionStep);
+    }
+    showGrid();
+}
+
+main();
