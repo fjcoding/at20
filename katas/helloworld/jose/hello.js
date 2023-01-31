16a4f311d4a184b4f061dfd5ee15e20eeac4197f
Jose Guevara <Jose.Guevara@Jala.University>
2023-01-31 00:45:16 -0400

    Add jest dependency for running unit tests and test sample for hello world kata

--- katas/helloworld/jose/hello.js
+++ katas/helloworld/jose/hello.js
@@ -1,7 +1,4 @@
 // read arguments from console
-const arg1 = process.argv[2]; // first argument
-const arg2 = process.argv[3]; // second argument
-
-console.log('hello world...!');
-console.log(`First arg: ${arg1}`);
-console.log(`Second arg: ${arg2}`);
+export function sayHello() {
+    return 'hello world...!';
+}
