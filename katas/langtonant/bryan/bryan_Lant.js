c7025cbf87ef035d9af72c56c283002bec326f37
Bryan Colque Clavijo <Bryan.Colque@jala.university>
2023-02-01 12:52:26 -0400

    Changes added and all tests approved in bryan_Lant file

--- katas/langtonant/bryan/bryan_Lant.js
+++ katas/langtonant/bryan/bryan_Lant.js
@@ -1,44 +1,21 @@
 
 
-var antUp = 0; //fixed direction value for up
-var antR = 1;//fixed direction value for right
-var antD = 2;//fixed direction value for down
-var antL = 3;//fixed direction value for left
-
-var antDir = antUp;//Default initial direction
-var stateCell;//State that will be read when switching positions
-var antNdir;
-
-var bSt = '*';
-var wSt = '-';
-
-var steps;
-//var maxSteps = 11000;
-
-var height = 70;
-var width = 70;
-var antX = width / 2;//ant's x coordinate subject to change
-var antY = height / 2;// //Given a 70x70 canvas, the coordinates start in the middle
-
-var step = 1;
-var canvas = [];
-
-export function makeGrid(a, b) {
+ export function makeGrid(aMg, bMg) {
     var grid = [];
-    for (var iMg = 0; iMg < a; iMg++) {
-        for (var jMg = 0; jMg < b; jMg++) {
+    for (var iMg = 0; iMg < aMg; iMg++) {
+        for (var jMg = 0; jMg < bMg; jMg++) {
             grid[iMg] = [];
         }
     }
-    for (var iMg2 = 0; iMg2 < a; iMg2++) {
-        for (var jMg2 = 0; jMg2 < b; jMg2++) {
+    for (var iMg2 = 0; iMg2 < aMg; iMg2++) {
+        for (var jMg2 = 0; jMg2 < bMg; jMg2++) {
             grid[iMg2][jMg2] = '-';
         }
     }
     return grid;
 }
 
-function displayGrid(gridInt) {
+ export function displayGrid(gridInt) {
     var display = '';
 
     var sizeY = gridInt.length;
@@ -60,49 +37,93 @@ export function cellState(gridState, cellY, cellX) {
     return state;
 }
 
-export function direction(state) {
-    if (state == wSt) {
+ export function direction(state, antDir) { //check for initial position
+    var antUpd = 0; //fixed direction value for up
+    var antLd = 3;//fixed direction value for left
+    var bStd = '*';//black state
+    var wStd = '-';//white state
+
+    if (state == wStd) {
         antDir += 1;
         if (antDir > 3) {
-            antDir = antUp;
+            antDir = antUpd;
         }
+    }
 
-        canvas[antY][antX] = '*';
-    } else if (state == bSt) {
+    else if (state == bStd) {
         antDir -= 1;
         if (antDir < 0) {
-            antDir = antL;
+            antDir = antLd;
         }
 
-        canvas[antY][antX] = '-';
+       
     }
     return antDir;
 }
 
+ export function swState(state) {
+    var bStsw = '*';//black state
+    var wStsw = '-';//white state
+    if (state == wStsw) {
+        state=bStsw;
+        
+    }
 
-export function move(dir) {
+    else if (state == bStsw) {
+       state=wStsw;
+       
+    }
+
+    return state;
+ }
+
+export function move(dir,antYm,antXm) {
+
+    var antUp = 0;
+    var antR = 1;
+    var antD = 2;
+    var antL = 3;
+
+    var step = 1;
+
     if (dir == antUp) {
-        antY -= step;
+        antYm -= step;
     } else if (dir == antR) {
-        antX += step;
+        antXm += step;
     } else if (dir == antD) {
-        antY += step;
+        antYm += step;
     } else if (dir == antL) {
-        antX -= step;
+        antXm -= step;
     }
+    return [antYm,antXm];
 }
 
 
-function show(max) {
-    canvas = makeGrid(height, width);//for the coordinates, switched coordinates to match the sample canvas
+export function langtonAnt(max,height,width,inDir) { 
+    
+    var canvas = [];
+    canvas = makeGrid(height, width);
+    var nState;
+    var stateCell;
+    var antYlan = height/2;
+    var antXlan = width/2;
+    var antNdir = inDir;
 
-    for (steps = 0; steps <= max; steps++) {
-        stateCell = cellState(canvas, antY, antX); //add a line where we can read the state of the cell in the new coordinates after the function move
-        antNdir = direction(stateCell);
-        move(antNdir);
+    for (var steps = 0; steps <= max; steps++) {
+
+        stateCell = cellState(canvas, antYlan, antXlan);
+        antNdir = direction(stateCell, antNdir);
+        nState=swState(stateCell);
+        canvas[antYlan][antXlan]=nState;
+        [antYlan,antXlan]=move(antNdir,antYlan,antXlan);
     }
-    console.log(displayGrid(canvas));
+    return displayGrid(canvas);
+    //console.log(displayGrid(canvas));
 }
 
-var maxSteps = process.argv[2];
-show(maxSteps);
+
+// var maxSteps = process.argv[2];
+// var hIght = process.argv[3];
+// var wDth = process.argv[4];
+//  var nDir = 1;
+// langtonAnt(maxSteps,hIght,wDth,nDir);
