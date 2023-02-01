5818ad3b7469c7a1a1cbd485a93aa5a71a451328
Giovanni Blanco <Giovanni.Blanco@jala.university>
2023-02-01 13:47:34 -0400

    Fixing issues and completing the tests

--- katas/langtonant/Giovanni/langtonsant.js
+++ katas/langtonant/Giovanni/langtonsant.js
@@ -1,9 +1,7 @@
 export const mapDim = 6;
-
-let map = new Array(mapDim).fill().map(() => Array(mapDim).fill(0));
-
 export const ant = {};
 
+const steps = 10;
 const up = 0;
 const right = 1;
 const down = 2;
@@ -14,26 +12,30 @@ const white = 0;
 
 const turnLeft = 3;
 const turnRight = 1;
-
 ant.symbol = 8;
-ant.x = mapDim / 2;
-ant.y = mapDim / 2;
-ant.dir = left;
 
 export function turn(color) {
     switch (color) {
     case white:
-        map[ant.y][ant.x] = 1;
         ant.dir = (ant.dir + turnRight) % 4;
-        map[ant.y][ant.x] = black;
         break;
     case black:
-        map[ant.y][ant.x] = 0;
         ant.dir = (ant.dir + turnLeft) % 4;
-        map[ant.y][ant.x] = white;
         break;
     }
     return (ant.dir);
+}
+
+export function changeMap(color, map) {
+    switch (color) {
+    case white:
+        map[ant.y][ant.x] = black;
+        break;
+    case black:
+        map[ant.y][ant.x] = white;
+        break;
+    }
+    return (map);
 }
 
 
@@ -52,17 +54,26 @@ export function advance() {
         ant.x--;
         break;
     }
-    return (ant.x, ant.y);
+    const array = [ant.x, ant.y];
+    return (array);
 }
 
+export function fullyFunctionallity(steps, mapDim, dir) {
+    let map = new Array(mapDim).fill().map(() => Array(mapDim).fill(0));
+    ant.x = mapDim / 2;
+    ant.y = mapDim / 2;
+    ant.dir = dir;
+    for (let iterations = 0; iterations < steps; iterations++) {
+        let color = map[ant.y][ant.x];
+        map[ant.y][ant.x] = ant.symbol;
 
-for (let iterations = 0; iterations < 10; iterations++) {
-    let color = map[ant.y][ant.x];
+        turn(color);
+        changeMap(color, map);
+        advance();
+    }
     map[ant.y][ant.x] = ant.symbol;
-    console.log(map);
-
-    turn(color);
-    advance();
+    return (map);
 }
-map[ant.y][ant.x] = ant.symbol;
-console.log(map);
+
+
+fullyFunctionallity(steps, mapDim, left);
