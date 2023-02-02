f8e3f0981d1a435ad535b5574be4ba5525ab1620
Adalid Perez <Adalid.Perez@jala.university>
2023-02-01 21:51:18 -0400

    Refactor langtonant.js file, main.js file and add maintest.js file

--- katas/langtonant/adalid/langtonant.js
+++ katas/langtonant/adalid/langtonant.js
@@ -1,129 +1,82 @@
-import { showTable } from './table.js';
-
-const antMoves = (matriz = [[]], steps = undefined, direction = '', posX, posY, posBeforeA) => {
+const positionInGrid = ({ direction, currentPos, posX, posY, matriz }) => {
     const BLACK = 0;
     const WHITE = 1;
     const UP = 'U';
     const RIGHT = 'R';
     const LEFT = 'L';
     const DOWN = 'D';
-    if (posBeforeA === BLACK) {
-        if (direction === UP) {
-            posBeforeA = matriz[posX][posY - 1];
-            console.log(posBeforeA);
-            matriz[posX][posY - 1] = 'a';
-            matriz[posX][posY] = WHITE;
-            steps--;
-            direction = LEFT;
-            console.log('New direction ' + direction, steps);
-            console.log('**********');
-            showTable(matriz);
-            if (steps > 0) {
-                antMoves(matriz, steps, direction, posX, posY, posBeforeA);
-            }
-        } else
-        if (direction === LEFT) {
-            posBeforeA = matriz[posX + 1][posY];
-            console.log(posBeforeA);
-            matriz[posX + 1][posY - 1] = 'a';
-            matriz[posX][posY - 1] = WHITE;
-            steps--;
-            direction = DOWN;
 
-            console.log('New direction ' + direction, steps);
-            console.log('**********');
-            showTable(matriz);
-            if (steps > 0) {
-                antMoves(matriz, steps, direction, posX, posY, posBeforeA);
-            }
-        } else
-        if (direction === DOWN) {
-            posBeforeA = matriz[posX][posY + 1];
-            console.log(posBeforeA);
-            matriz[posX + 1][posY] = 'a';
-            matriz[posX + 1][posY - 1] = WHITE;
-            steps--;
-            direction = RIGHT;
-            console.log('New direction ' + direction, steps);
-            console.log('**********');
-            showTable(matriz);
-            if (steps > 0) {
-                antMoves(matriz, steps, direction, posX, posY, posBeforeA);
-            }
-        } else
-        if (direction === RIGHT) {
-            posBeforeA = matriz[posX][posY - 1];//1
-            console.log(posBeforeA);
-            matriz[posX][posY] = 'a';
-            matriz[posX + 1][posY] = WHITE;
-            steps--;
-            direction = UP;
-            console.log('New direction ' + direction, steps);
-            console.log('**********');
-            showTable(matriz);
-            if (steps > 0) {
-                antMoves(matriz, steps, direction, posX, posY, posBeforeA);
-            }
-        }
-    }
-    //Grid WHITE
-    if (posBeforeA === WHITE) {
-        if (direction === RIGHT) {
-            posBeforeA = matriz[posX][posY + 1];
-            console.log(posBeforeA);
-            matriz[posX + 1][posY + 1] = 'a';
-            matriz[posX][posY + 1] = BLACK;
-            steps--;
-            direction = DOWN;
-            console.log('New direction ' + direction, steps);
-            console.log('**********');
-            showTable(matriz);
-            if (steps > 0) {
-                antMoves(matriz, steps, direction, posX, posY, posBeforeA);
-            }
-        } else
-        if (direction === UP) {
-            posBeforeA = matriz[posX][posY + 1];//1
-            console.log(posBeforeA);
-            matriz[posX][posY + 1] = 'a';
-            matriz[posX][posY] = BLACK;
-            steps--;
-            direction = RIGHT;
-            console.log('New direction ' + direction, steps);
-            console.log('**********');
-            showTable(matriz);
-            if (steps > 0) {
-                antMoves(matriz, steps, direction, posX, posY, posBeforeA);
-            }
-        } else
-        if (direction === LEFT) {
-            posBeforeA = matriz[posX + 1][posY];
-            console.log(posBeforeA);
-            matriz[posX + 1][posY - 1] = 'a';
-            matriz[posX][posY - 1] = BLACK;
-            steps--;
-            direction = UP;
-            console.log('New direction ' + direction, steps);
-            console.log('**********');
-            showTable(matriz);
-            if (steps > 0) {
-                antMoves(matriz, steps, direction, posX, posY, posBeforeA);
-            }
-        } else
-        if (direction === DOWN) {
-            posBeforeA = matriz[posX][posY + 1];
-            console.log(posBeforeA);
-            matriz[posX + 1][posY] = 'a';
-            matriz[posX + 1][posY - 1] = BLACK;
-            steps--;
+    switch (direction) {
+    case UP:
+        if (currentPos === BLACK) {
+            posY--;
+            currentPos = WHITE;
+            currentPos = matriz[posX][posY];
             direction = LEFT;
-            console.log('New direction ' + direction, steps);
-            console.log('**********');
-            showTable(matriz);
-            if (steps > 0) {
-                antMoves(matriz, steps, direction, posX, posY, posBeforeA);
-            }
+        } else
+        if (currentPos === WHITE) {
+            posY++;
+            currentPos = BLACK;
+            currentPos = matriz[posX][posY];
+            direction = RIGHT;
         }
+        break;
+    case LEFT:
+        if (currentPos === BLACK) {
+            posX++;
+            currentPos = WHITE;
+            currentPos = matriz[posX][posY];
+            direction = DOWN;
+        } else
+        if (currentPos === WHITE) {
+            posX--;
+            currentPos = BLACK;
+            currentPos = matriz[posX][posY];
+            direction = UP;
+        }
+        break;
+    case RIGHT:
+        if (currentPos === BLACK) {
+            posX--;
+            currentPos = WHITE;
+            currentPos = matriz[posX][posY];
+            direction = UP;
+        } else
+        if (currentPos === WHITE) {
+            posX++;
+            currentPos = BLACK;
+            currentPos = matriz[posX][posY];
+            direction = DOWN;
+        }
+        break;
+    case DOWN:
+        if (currentPos === BLACK) {
+            posY++;
+            currentPos = WHITE;
+            currentPos = matriz[posX][posY];
+            direction = RIGHT;
+        } else
+        if (currentPos === WHITE) {
+            posY--;
+            currentPos = BLACK;
+            currentPos = matriz[posX][posY];
+            direction = LEFT;
+        }
+        break;
+    default:
+        break;
     }
+    return { posX, posY, direction };
 };
-export {antMoves};
+
+const antSteps = (steps, matriz, direction, posX, posY, currentPos) =>{
+    let mutatedVariables = { matriz, direction, posX, posY, currentPos };
+
+    while (steps > 0) {
+        const newPositions = positionInGrid(mutatedVariables);
+        Object.assign(mutatedVariables, newPositions);
+        steps--;
+    }
+    return mutatedVariables;
+};
+export {positionInGrid, antSteps};
