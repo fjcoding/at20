234497dd39bdb7ede232411b9634c1170aa61e41
Charles Flores <Charles.Flores@jala.university>
2023-02-02 02:16:42 -0400

    Unit tests performed and fixed issues to langton ant code

--- katas/langtonant/charles/langtonant.js
+++ katas/langtonant/charles/langtonant.js
@@ -1,5 +1,5 @@
-const arg1 = process.argv[2];//N dimension grid
-const arg2 = process.argv[3];//N steps
+const arg1 = 10;//process.argv[2];//N dimension grid
+const arg2 = 10;//process.argv[3];//N steps
 
 const UP = '▲';
 const DOWN = '▼';
@@ -7,59 +7,49 @@ const RIGHT = '►';
 const LEFT = '◄';
 const WHITE = ' ';
 const BLACK = '■';
-let INITIALPOSITION;
 
-function createGrid(dimension) {
+export function createGrid(dimension) {
     let grid = new Array(dimension);
     for (let rowIndex = 0; rowIndex < dimension; rowIndex++) {
         grid[rowIndex] = new Array(dimension);
         for (let colIndex = 0; colIndex < dimension; colIndex++) {
-            grid[rowIndex][colIndex] = {
-                valueBox: WHITE,
-                isPaintedNextPosition: false
-            };
+            grid[rowIndex][colIndex] = { valueBox: WHITE, isPaintedNextPosition: false };
         }
     }
     return grid;
 }
 
-function initialPositionDirection(grid, directionStep) {
-    const dimension = grid.length;
-    INITIALPOSITION = dimension % 2 == 0 ? (dimension) / 2 - 1 : (dimension) / 2 - 0.5;
-    grid[INITIALPOSITION][INITIALPOSITION].valueBox = directionStep;
-    return grid;
+export function initialPosition(dimension) {
+    let initialPos = dimension % 2 == 0 ? (dimension) / 2 - 1 : (dimension) / 2 - 0.5;
+    return initialPos;
 }
 
-function nextPosition(grid, rowPosition, colPosition) {
+export function nextPosition(grid, rowPosition, colPosition) {
     let rowIndex = 0;
     let colIndex = 0;
     let newDirection;
     switch (grid[rowPosition][colPosition].valueBox) {
-        case UP:
-            rowIndex--;
-            newDirection = !verifyPainted(grid[rowPosition + rowIndex][colPosition + colIndex]).isPaintedNextPosition ? RIGHT : LEFT;
-            break;
-        case DOWN:
-            rowIndex++;
-            newDirection = !verifyPainted(grid[rowPosition + rowIndex][colPosition + colIndex]).isPaintedNextPosition ? LEFT : RIGHT;
-            break;
-        case RIGHT:
-            colIndex++;
-            newDirection = !verifyPainted(grid[rowPosition + rowIndex][colPosition + colIndex]).isPaintedNextPosition ? DOWN : UP;
-            break;
-        case LEFT:
-            colIndex--;
-            newDirection = !verifyPainted(grid[rowPosition + rowIndex][colPosition + colIndex]).isPaintedNextPosition ? UP : DOWN;
-            break;
+    case UP:
+        rowIndex--;
+        newDirection = !verifyPainted(grid[rowPosition + rowIndex][colPosition + colIndex]).isPaintedNextPosition ? RIGHT : LEFT;
+        break;
+    case DOWN:
+        rowIndex++;
+        newDirection = !verifyPainted(grid[rowPosition + rowIndex][colPosition + colIndex]).isPaintedNextPosition ? LEFT : RIGHT;
+        break;
+    case RIGHT:
+        colIndex++;
+        newDirection = !verifyPainted(grid[rowPosition + rowIndex][colPosition + colIndex]).isPaintedNextPosition ? DOWN : UP;
+        break;
+    case LEFT:
+        colIndex--;
+        newDirection = !verifyPainted(grid[rowPosition + rowIndex][colPosition + colIndex]).isPaintedNextPosition ? UP : DOWN;
+        break;
     }
-    return {
-        rowIndex,
-        colIndex,
-        newDirection
-    };
+    return { rowIndex, colIndex, newDirection };
 }
 
-function updateValues(grid, rowPosition, colPosition, newPosition) {
+export function updateValues(grid, rowPosition, colPosition, newPosition) {
     rowPosition += newPosition.rowIndex;
     colPosition += newPosition.colIndex;
     grid[rowPosition][colPosition] = verifyPainted(grid[rowPosition][colPosition]);
@@ -67,7 +57,7 @@ function updateValues(grid, rowPosition, colPosition, newPosition) {
     return { grid, rowPosition, colPosition };
 }
 
-function verifyPainted(nextPosition) {
+export function verifyPainted(nextPosition) {
     if (nextPosition.valueBox == BLACK) {
         nextPosition.isPaintedNextPosition = true;
     } else {
@@ -76,7 +66,7 @@ function verifyPainted(nextPosition) {
     return nextPosition;
 }
 
-function changeColor(currentPosition) {
+export function changeColor(currentPosition) {
     if (!currentPosition.isPaintedNextPosition) {
         currentPosition.valueBox = BLACK;
     } else {
@@ -85,29 +75,33 @@ function changeColor(currentPosition) {
     return currentPosition;
 }
 
-function showGrid(grid) {
-    let displayContent = '';
-    for (let indexR = 0; indexR < grid.length; indexR++) {
-        for (let indexC = 0; indexC < grid[indexR].length; indexC++) {
-            displayContent += grid[indexR][indexC].valueBox + ' ';
-        }
-        displayContent += '\n';
+// function showGrid(grid) {
+//     let displayContent = '';
+//     for (let indexR = 0; indexR < grid.length; indexR++) {
+//         for (let indexC = 0; indexC < grid[indexR].length; indexC++) {
+//             displayContent += grid[indexR][indexC].valueBox + ' ';
+//         }
+//         displayContent += '\n';
+//     }
+//     console.log(displayContent);
+// }
+
+export function antPath(arg1, arg2) {
+    let grid = createGrid(arg1);
+    let rowPosition = initialPosition(arg1);
+    let colPosition = initialPosition(arg1);
+    let valueInitial = {valueBox: UP, isPaintedNextPosition: false };
+    grid[rowPosition][colPosition] = valueInitial;
+    for (let index = 0; index < arg2; index++) {
+        let newPosition = nextPosition(grid, rowPosition, colPosition);
+        grid[rowPosition][colPosition] = changeColor(grid[rowPosition][colPosition]);
+        let gridUpdated = updateValues(grid, rowPosition, colPosition, newPosition);
+        grid = gridUpdated.grid;
+        rowPosition = gridUpdated.rowPosition;
+        colPosition = gridUpdated.colPosition;
     }
-    return displayContent;
+    return grid;
 }
 
-let grid = createGrid(arg1);
-grid = initialPositionDirection(grid, UP);
-let rowPosition = INITIALPOSITION;
-let colPosition = INITIALPOSITION;
-
-for (let index = 0; index < arg2; index++) {
-    let newPosition = nextPosition(grid, rowPosition, colPosition);
-    grid[rowPosition][colPosition] = changeColor(grid[rowPosition][colPosition]);
-    let gridUpdated = updateValues(grid, rowPosition, colPosition, newPosition);
-    grid = gridUpdated.grid;
-    rowPosition = gridUpdated.rowPosition;
-    colPosition = gridUpdated.colPosition;
-}
-
-console.log(showGrid(grid));
+antPath(arg1, arg2);
+// showGrid(antPath(arg1, arg2));
