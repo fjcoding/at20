3589995dfa5ad3474cad49b95722712c00df17ef
Williams Velasquez <Williams.Velasquez@jala.university>
2023-02-02 22:15:38 -0400

    Add file poker and poker.test to pokerhands

--- katas/pokerhands/williams/poker.js
+++ katas/pokerhands/williams/poker.js
@@ -1,39 +1,39 @@
 export function countRepeats(hand) {
     const repeat = {};
     hand.forEach(card => {
-      if (!repeat[card.value]) {
-        repeat[card.value] = 0;
-      }
-      repeat[card.value]++;
+        if (!repeat[card.value]) {
+            repeat[card.value] = 0;
+        }
+        repeat[card.value]++;
     });
     return repeat;
-  }
+}
 
 export function findPokerr(hand) {
     const repeat = countRepeats(hand);
     for (let value in repeat) {
-      if (repeat[value] === 4) {
-        return parseInt(value, 10);
-      }
+        if (repeat[value] === 4) {
+            return parseInt(value, 10);
+        }
     }
     return null;
-  }
-  
-  const hand = [
+}
+
+const hand = [
     { value: 8, stick: 'diamonds' },
     { value: 8, stick: 'hearts' },
     { value: 8, stick: 'clubs' },
     { value: 8, stick: 'spades' },
     { value: 4, stick: 'diamonds' }
-  ];
+];
 
-  console.log(findPokerr(hand));
+console.log(findPokerr(hand));
 
-  const hand1 = [
+const hand1 = [
     { value: 8, stick: 'diamonds' },
     { value: 7, stick: 'hearts' },
     { value: 8, stick: 'clubs' },
     { value: 8, stick: 'spades' },
     { value: 4, stick: 'diamonds' }
-  ];
-  console.log(findPokerr(hand1));
+];
+console.log(findPokerr(hand1));
--- katas/pokerhands/williams/poker.test.js
+++ katas/pokerhands/williams/poker.test.js
@@ -1,12 +1,12 @@
-import { findPokerr, countRepeats} from './poker';
+import { findPokerr} from './poker';
 describe('findPokerr', () => {
     it('should return the value of the card that repeats 4 times in the hand', () => {
-      const hand = [{ value: 2 },{ value: 3 },{ value: 2 },{ value: 2 },{ value: 2 },];
-      expect(findPokerr(hand)).toBe(2);
+        const hand = [{ value: 2 }, { value: 3 }, { value: 2 }, { value: 2 }, { value: 2 },];
+        expect(findPokerr(hand)).toBe(2);
     });
-  
+
     it('should return null if no card repeats 4 times in the hand', () => {
-      const hand = [{ value: 2 },{ value: 3 },{ value: 4 }, { value: 5 },{ value: 6 },];
-      expect(findPokerr(hand)).toBe(null);
+        const hand = [{ value: 2 }, { value: 3 }, { value: 4 }, { value: 5 }, { value: 6 },];
+        expect(findPokerr(hand)).toBe(null);
     });
-  });
+});
