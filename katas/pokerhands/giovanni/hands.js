a47cd29817d911e7ad750347b295f410b3c2b060
Giovanni Blanco <Giovanni.Blanco@jala.university>
2023-02-03 19:32:58 -0400

    Add rankHand method in hands class, but is incomplete

--- katas/pokerhands/giovanni/hands.js
+++ katas/pokerhands/giovanni/hands.js
@@ -29,18 +29,18 @@ export class Hands {
     }
 
     distribution() {
-        let dist = Array.apply(null, Array(13)).map(() => 0);
+        let dist = Array.apply(null, Array(14)).map(() => 0);
         const cards = this.separateCards();
         for (let i = 0; i < cards.length; i++) {
             let compareCard = new Card(cards[i]);
-            dist[compareCard.value() - 2] += 1;
+            dist[compareCard.value() - 1] += 1;
         }
         return dist;
     }
 
     countPosition(num) {
         const dist = this.distribution();
-        for (let i = 0; i < 13; i++) {
+        for (let i = 1; i < 14; i++) {
             if (dist[i] == num) {
                 return i;
             }
@@ -51,7 +51,7 @@ export class Hands {
     highStraightCard() {
         const dist = this.distribution();
         let cont = 0;
-        for (let i = 0; i < dist.length; i++) {
+        for (let i = 1; i < dist.length; i++) {
             for (let j = 0; j < 4; j++) {
                 if (dist[i + j] == 1) {
                     cont++;
@@ -64,5 +64,34 @@ export class Hands {
             }
         }
         return false;
+    }
+
+    handRank() {
+        if (this.highStraightCard() != false) {
+            if (this.isFlush()) {
+                return 8;
+            }
+        }
+        if (this.countPosition(4) != false) {
+            return 7;
+        }
+        if (this.countPosition(3) != false) {
+            if (this.countPosition(2) != false) {
+                return 6;
+            }
+        }
+        if (this.isFlush()) {
+            return 5;
+        }
+        if (this.highStraightCard() != false) {
+            return 4;
+        }
+        if (this.countPosition(3) != false) {
+            return 3;
+        }
+        if (this.countPosition(2) != false) {
+            return 1;
+        }
+        return 0;
     }
 }
--- katas/pokerhands/giovanni/hands.test.js
+++ katas/pokerhands/giovanni/hands.test.js
@@ -15,16 +15,16 @@ describe('Test for Hands class', () => {
     it('should be able to obtain a distribution vector of the cards from a hand', () => {
         const testhand = '2C 3C 4C 8C AC';
         const Hand = new Hands(testhand);
-        // card values        [2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A]
-        //distribution vector [0, 0, 0, 0, 0, 0, 1, 0,  0, 0, 0, 0, 0]
-        const resultDist = [1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1];
+        // card values        [nan, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A]
+        //distribution vector [0,   0, 0, 0, 0, 0, 0, 1, 0,  0, 0, 0, 0, 0]
+        const resultDist = [0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1];
         expect(Hand.distribution()).toEqual(resultDist);
     });
-    it('should be able to obtain the place in distrigution when we found a number of cards', () => {
+    it('should be able to obtain the place in distribution when we found a number of cards', () => {
         const testhand = '2C 2H 2C 8C AC';
         const Hand = new Hands(testhand);
-        //testHand distribution result[3, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1]
-        const resultPos = 0; //Position when we found 3 cards
+        //testHand distribution result[0, 3, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1]
+        const resultPos = 1; //Position when we found 3 cards
         expect(Hand.countPosition(3)).toEqual(resultPos);
         expect(Hand.countPosition(2)).toEqual(false);
         expect(Hand.countPosition(4)).toEqual(false);
@@ -32,7 +32,56 @@ describe('Test for Hands class', () => {
     it('should be able to obtain the place, in the distribution, of the highest card in a straight of four cards', () => {
         const testhand = '2H 7H 6S 4D, 5S';
         const Hand = new Hands(testhand);
-        const result = 5;
+        const result = 6;
         expect(Hand.highStraightCard()).toEqual(result);
+    });
+    //At this point the hands are ranked in scale of 0 to 8
+    it('should be able to return a value  8 that represents the rank of the hand', () => {
+        const testhand = 'AS 10S QS KS JS';
+        const Hand = new Hands(testhand);
+        const rank = 8;
+        expect(Hand.handRank()).toEqual(rank);
+    });
+    it('should be able to return a value  7 that represents the rank of the hand', () => {
+        const testhand = '7H 7C 7D 3D 7S';
+        const Hand = new Hands(testhand);
+        const rank = 7;
+        expect(Hand.handRank()).toEqual(rank);
+    });
+    it('should be able to return a value  6 that represents the rank of the hand', () => {
+        const testhand = '5C 8D 5H 8S 8H';
+        const Hand = new Hands(testhand);
+        const rank = 6;
+        expect(Hand.handRank()).toEqual(rank);
+    });
+    it('should be able to return a value  5 that represents the rank of the hand', () => {
+        const testhand = 'AC 5C 3C 10C 7C';
+        const Hand = new Hands(testhand);
+        const rank = 5;
+        expect(Hand.handRank()).toEqual(rank);
+    });
+    it('should be able to return a value  4 that represents the rank of the hand', () => {
+        const testhand = '3H 7H 6S 4D 5S';
+        const Hand = new Hands(testhand);
+        const rank = 4;
+        expect(Hand.handRank()).toEqual(rank);
+    });
+    it('should be able to return a value  3 that represents the rank of the hand', () => {
+        const testhand = '5S 4C AD 4S 4H';
+        const Hand = new Hands(testhand);
+        const rank = 3;
+        expect(Hand.handRank()).toEqual(rank);
+    });
+    it('should be able to return a value  1 that represents the rank of the hand', () => {
+        const testhand = '2C 3D 4S 5H 2D';
+        const Hand = new Hands(testhand);
+        const rank = 1;
+        expect(Hand.handRank()).toEqual(rank);
+    });
+    it('should be able to return a value  0 that represents the rank of the hand', () => {
+        const testhand = 'AC 5C 10C 7C 3S';
+        const Hand = new Hands(testhand);
+        const rank = 0;
+        expect(Hand.handRank()).toEqual(rank);
     });
 });
