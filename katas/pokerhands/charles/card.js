49887d2fc012254d4cc0baeb01142ad0d7b3ab03
Charles Flores <Charles.Flores@jala.university>
2023-02-02 23:13:59 -0400

    Add charles folder, add card.test.js and card.js files and create unit tests to Card class

---
+++ katas/pokerhands/charles/card.js
@@ -0,0 +1,21 @@
+export class Card {
+    static allowedValues = ['2','3','4','5','6','7','8','9','T','J','Q','K','A'];
+    static allowedSuits = ['C','D','H','S'];
+
+    #value;
+
+    #suit;
+
+    constructor(card) {
+        this.#value = Card.allowedValues.indexOf(card.charAt(0)) >= 0 ? card.charAt(0) : '-1';
+        this.#suit = Card.allowedSuits.indexOf(card.charAt(1)) >= 0 ? card.charAt(1) : '-1';
+    }
+
+    getValueCard() {
+        return this.#value;
+    }
+
+    getSuitCard() {
+        return this.#suit;
+    }
+}
