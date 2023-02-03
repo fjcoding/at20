77c079f1de19aa1a3524f381d8c4374641fc78ec
Lorena Ramirez <Lorena.Ramirez@jala.university>
2023-02-03 11:47:03 -0500

    Add recognition value and suite of a card

--- katas/pokerhands/lorena/pokerhands.test.js
+++ katas/pokerhands/lorena/pokerhands.test.js
@@ -7,4 +7,19 @@ describe('Test for Card class', () => {
         const card = new Card(value, suite);
         expect(card.cardREad()).toBe(value + suite);
     });
+
+    it('should be able identify card`s value', () => {
+        const value = '7';
+        const suite = 'D';
+        const card = new Card(value, suite);
+        expect(card.cardREad()[0]).toBe(value);
+    });
+
+    it('should be able identify card`s suite', () => {
+        const value = '7';
+        const suite = 'D';
+        const card = new Card(value, suite);
+        expect(card.cardREad()[1]).toBe(suite);
+    });
 });
+
