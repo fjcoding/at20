eaf9f80c6df6047c3589c3481b9cab82932d849e
Giovanni Blanco <Giovanni.Blanco@jala.university>
2023-02-02 21:08:41 -0400

    add hands class files

---
+++ katas/pokerhands/giovanni/hands.js
@@ -0,0 +1,13 @@
+export class Hands {
+    #handString;
+
+    constructor(handString) {
+        this.#handString = handString;
+    }
+
+    separateCards() {
+        const hand = this.#handString;
+        const cards = ['2C', '3H', '4S', '8C', 'AH'];
+        return cards;
+    }
+}
---
+++ katas/pokerhands/giovanni/hands.test.js
@@ -0,0 +1,10 @@
+import { Hands } from './hands';
+
+describe('Test for Hands class', () => {
+    it('should be able to obtain an array of separate string cards from a hand', () => {
+        const testhand = '2C 3H 4S 8C AH';
+        const resultHand = ['2C', '3H', '4S', '8C', 'AH'];
+        const Hands = new Hands(testhand);
+        expect(Hands.separateCards()).toEqual(resultHand);
+    });
+});
