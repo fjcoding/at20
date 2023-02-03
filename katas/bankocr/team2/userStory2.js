e517ab6da68195a120346bead2b6c84036d9d615
Daniel Carrasco <Daniel.Carrasco@jala.university>
2023-02-03 08:47:44 -0400

    Added the first class to the new file to read number

---
+++ katas/bankocr/team2/userStory2.js
@@ -0,0 +1,3 @@
+export class verifNumber(numberAccount) { 
+
+      }
---
+++ katas/bankocr/team2/userStory2.test.js
@@ -0,0 +1,8 @@
+import {AccountNumber}from './accnum';
+
+describe('Tests for verifNumber funtion', () => { 
+      it('should be able to receive three lines and generate digits for account number', () => { 
+        const entryNum = 345882865;
+        const accountNum = new AccountNumber ()
+        expect ()
+     });});
