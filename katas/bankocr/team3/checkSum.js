113477d801f4443a60493c0164a5e22ba7ccb57f
Lorena Ramirez <Lorena.Ramirez@jala.university>
2023-02-03 08:33:23 -0500

    add identify same number with 9 digits

--- katas/bankocr/team3/checkSum.js
+++ katas/bankocr/team3/checkSum.js
@@ -1,7 +1,21 @@
 import { asNumber } from './accnum';
+
 export class checkSum{
     #lastNumber
     constructor(lastNumber) {
         this.#lastNumber=lastNumber;
     }
+    sameNum(){
+        return this.#lastNumber;
+        }
+
+    outSum () {
+         const numNew = String(this.#lastNumber);
+         const size = numNew.length;
+         var suma = 0;
+         for (let i = (size - 1); i >= 0; i-- ) {
+                suma = suma + numNew[i];
+         }
+    }
+
 }
--- katas/bankocr/team3/checkSum.test.js
+++ katas/bankocr/team3/checkSum.test.js
@@ -3,7 +3,7 @@ import {checkSum} from './checkSum';
 test('verify checksum', () => {
     const numTest = 576516546;
     let sumResult = new checkSum(numTest);
-    expect(sumResult).toBe(0);
+    expect(sumResult.sameNum()).toBe(576516546);
 });
 
 
