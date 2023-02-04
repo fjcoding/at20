026c80583f8801707b6279fdc1749760e980734b
Charles Flores <Charles.Flores@jala.university>
2023-02-03 22:25:17 -0400

    Add functionality to find a three of kind and straight in poker hand

--- katas/pokerhands/charles/pokerHand.js
+++ katas/pokerhands/charles/pokerHand.js
@@ -2,44 +2,50 @@ import { Card } from './card';
 
 export class PokerHand {
     static valueCards = {
-        2: 1,
-        3: 2,
-        4: 3,
-        5: 4,
-        6: 5,
-        7: 6,
-        8: 7,
-        9: 8,
-        T: 9,
-        J: 10,
-        Q: 11,
-        K: 12,
-        A: 13
+        '2': 2,
+        '3': 3,
+        '4': 4,
+        '5': 5,
+        '6': 6,
+        '7': 7,
+        '8': 8,
+        '9': 9,
+        'T': 10,
+        'J': 11,
+        'Q': 12,
+        'K': 13,
+        'A': 14
     };
 
     #cards;
+
     #pairs;
+
+    #threeKind;
+
+    #straight;
+
+    #flush;
+
+    #fullHouse;
+
+    #fourKind;
+
+    #straightFlush;
 
     constructor(pokerHand) {
         this.#cards = [];
         pokerHand.forEach(card => {
             this.#cards.push(new Card(card));
         });
+        this.#threeKind = 0;
     }
 
     getCards() {
         return this.#cards;
     }
 
-    getPairs() {
-        return this.#pairs;
-    }
-
-    getCardsLength() {
-        return this.#cards.length;
-    }
-
-    countValues(currentCard, pokerHand) {
+    countEqualValues(currentCard, pokerHand) {
         let count = 0;
         pokerHand.forEach(card => {
             if (currentCard.getValueCard() == card.getValueCard()) {
@@ -49,10 +55,22 @@ export class PokerHand {
         return count;
     }
 
+    existPair(pair) {
+        let isExist = false;
+        this.#pairs.forEach(value => {
+            if (pair == value) {
+                isExist = true;
+            }
+        });
+        return isExist;
+    }
+
     isHighCard() {
-        let resp;
+        let resp = true;
         this.getCards().forEach(card => {
-            resp = this.countValues(card, this.getCards()) > 1 ? false : true;
+            if (this.countEqualValues(card, this.getCards()) != 1) {
+                resp = false;
+            }
         });
         return resp;
     }
@@ -60,8 +78,8 @@ export class PokerHand {
     countPairs() {
         this.#pairs = [];
         this.getCards().forEach(card => {
-            if(this.countValues(card, this.getCards()) == 2) {
-                if(!this.existPair(card.getValueCard())){
+            if (this.countEqualValues(card, this.getCards()) == 2) {
+                if (!this.existPair(card.getValueCard())) {
                     this.#pairs.push(card.getValueCard());
                 }
             }
@@ -69,13 +87,74 @@ export class PokerHand {
         return this.#pairs.length;
     }
 
-    existPair(pair) {
-        let isExist = false;
-        this.#pairs.forEach( value => {
-            if(pair == value) {
-                isExist = true;
+    isThreeKind() {
+        this.getCards().forEach(card => {
+            if (this.countEqualValues(card, this.getCards()) == 3) {
+                this.#threeKind = card.getValueCard();
             }
         });
-        return isExist;
+        return parseInt(this.#threeKind);
+    }
+
+    isStraight() {
+        let cardsOrdered = '';
+        const especialCase = 'A5432';
+        this.ordering();
+        this.getCards().forEach(card => {
+            cardsOrdered += card.getValueCard();
+        });
+        const firstValue = cardsOrdered.charAt(0);
+        const idealOrder = this.generateSecuence(firstValue);
+        return cardsOrdered == idealOrder || cardsOrdered == especialCase ? true : false;
+    }
+
+    generateSecuence(initialValue) {
+        let result = '';
+        let value = PokerHand.valueCards[initialValue];
+        for (let index = value; index > value - 5; index--) {
+            let aux = '';
+            switch (index) {
+            case 14:
+                aux = 'A';
+                break;
+            case 13:
+                aux = 'K';
+                break;
+            case 12:
+                aux = 'Q';
+                break;
+            case 11:
+                aux = 'J';
+                break;
+            case 10:
+                aux = 'T';
+                break;
+            default:
+                aux = PokerHand.valueCards[index];
+                break;
+            }
+            result += aux;
+        }
+        return result;
+    }
+
+    ordering() {
+        const lenCards = this.getCards().length;
+        for (let index = 1; index < lenCards; index++) {
+            for (let indexLeft = 0; indexLeft < (lenCards - index); indexLeft++) {
+                let indexRight = indexLeft + 1;
+                if (PokerHand.valueCards[this.getCards()[indexLeft].getValueCard()] < PokerHand.valueCards[this.getCards()[indexRight].getValueCard()]) {
+                    this.changeValues(indexLeft, indexRight);
+                }
+            }
+        }
+    }
+
+    changeValues(value1, value2) {
+        if (value1 != value2) {
+            const temp = this.getCards()[value1];
+            this.getCards()[value1] = this.getCards()[value2];
+            this.getCards()[value2] = temp;
+        }
     }
 }
--- katas/pokerhands/charles/pokerHand.test.js
+++ katas/pokerhands/charles/pokerHand.test.js
@@ -11,6 +11,11 @@ describe('Tests for hand class', () => {
         pokerHand = cardList.split(': ');
         hand = new PokerHand(pokerHand[1].split(' '));
         expect(hand.isHighCard()).toBe(false);
+
+        cardList = 'Black: 2H 3D 2S 2C 3H';
+        pokerHand = cardList.split(': ');
+        hand = new PokerHand(pokerHand[1].split(' '));
+        expect(hand.isHighCard()).toBe(false);
     });
 
     it('should be able to show how pairs there are in the poker hand', () => {
@@ -33,5 +38,55 @@ describe('Tests for hand class', () => {
         pokerHand = cardList.split(': ');
         hand = new PokerHand(pokerHand[1].split(' '));
         expect(hand.countPairs()).toBe(1);
+    });
+
+    it('should be able to show if there is three of a kind in the poker hand', () => {
+        let cardList = 'Black: 2H 3D 5S 9C KD';
+        let pokerHand = cardList.split(': ');
+        let hand = new PokerHand(pokerHand[1].split(' '));
+        expect(hand.isThreeKind()).toBe(0);
+
+        cardList = 'Black: 2H 3D 5S 2C KD';
+        pokerHand = cardList.split(': ');
+        hand = new PokerHand(pokerHand[1].split(' '));
+        expect(hand.isThreeKind()).toBe(0);
+
+        cardList = 'Black: 2H 3D 5S 2C 3H';
+        pokerHand = cardList.split(': ');
+        hand = new PokerHand(pokerHand[1].split(' '));
+        expect(hand.isThreeKind()).toBe(0);
+
+        cardList = 'Black: 2H 3D 2S 2C 3H';
+        pokerHand = cardList.split(': ');
+        hand = new PokerHand(pokerHand[1].split(' '));
+        expect(hand.isThreeKind()).toBe(2);
+    });
+
+    it('should be able to show if the poker hand is straight', () => {
+        let cardList = 'Black: 8H 3D 9S 5C 2D';
+        // let cardList = 'Black: 8H 6D 9S 5C 7D';
+        let pokerHand = cardList.split(': ');
+        let hand = new PokerHand(pokerHand[1].split(' '));
+        expect(hand.isStraight()).toBe(false);
+
+        cardList = 'Black: 9H AD TS 2C KD';
+        pokerHand = cardList.split(': ');
+        hand = new PokerHand(pokerHand[1].split(' '));
+        expect(hand.isStraight()).toBe(false);
+
+        cardList = 'Black: 2H 3D 5S 6C 4H';
+        pokerHand = cardList.split(': ');
+        hand = new PokerHand(pokerHand[1].split(' '));
+        expect(hand.isStraight()).toBe(true);
+
+        cardList = 'Black: JH TD AS KC QH';
+        pokerHand = cardList.split(': ');
+        hand = new PokerHand(pokerHand[1].split(' '));
+        expect(hand.isStraight()).toBe(true);
+
+        cardList = 'Black: 4H 2D AS 5C 3H';
+        pokerHand = cardList.split(': ');
+        hand = new PokerHand(pokerHand[1].split(' '));
+        expect(hand.isStraight()).toBe(true);
     });
 });
