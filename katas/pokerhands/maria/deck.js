72bb17993007f7b1c2ea84dc8b3709a735be3ced
Maria Huampo <Maria.Huampo@jala.university>
2023-02-04 00:28:50 -0400

    Add resolve to HighCard and advance paircard, to katas/pokerhand/maria folder

--- katas/pokerhands/maria/deck.js
+++ katas/pokerhands/maria/deck.js
@@ -1,74 +1,112 @@
-export class Deck{
+export class Deck {
     #deck;
+
     constructor(deck) {
         this.#deck = deck;
     }
 
     dealDeck() {
         var newDeck = this.#deck.split(' ');
-        return newDeck
+        return newDeck;
     }
 
     isHandOne(arrayCards) {
         var handOne = new Array();
         var arrayNumeber = ['2', '3', '4', '5', '6', '7', '8', '9'];
-        for (let i = 0; i < 5 ; i++){
-            var caracter = arrayCards[i+1].charAt(0);
-            if(arrayNumeber.includes(caracter) ){
-                handOne[i] = parseInt(caracter);
+        for (let i = 0; i < 5; i++) {
+            var caracter = arrayCards[i + 1].charAt(0);
+            if (arrayNumeber.includes(caracter)) {
+                handOne[i] = {value:parseInt(caracter), suit : arrayCards[i + 1].charAt(1)};
+            } else if (caracter == 'T') {
+                handOne[i] = {value:10, suit : arrayCards[i + 1].charAt(1)};
+            } else if (caracter == 'J') {
+                handOne[i] = {value:11, suit : arrayCards[i + 1].charAt(1)};
+            } else if (caracter == 'Q') {
+                handOne[i] = {value:12, suit : arrayCards[i + 1].charAt(1)};
+            } else if (caracter == 'K') {
+                handOne[i] = {value:13, suit : arrayCards[i + 1].charAt(1)};
+            } else {
+                handOne[i] = {value:14, suit : arrayCards[i + 1].charAt(1)};
             }
-            else if (caracter == 'T') {
-                handOne[i] = 10;
-            }
-            else if (caracter == 'J') {
-                handOne[i] = 11;
-            }
-            else if (caracter == 'Q') {
-                handOne[i] = 12;
-            }
-            else if (caracter == 'K') {
-                handOne[i] = 13;
-            }
-            else {
-                handOne[i] = 14;
-            }
-       
         }
 
-       return handOne
-    }  
+        return handOne;
+    }
 
-    isHandTwo(arrayCards){
+    isHandTwo(arrayCards) {
         var handTwo = new Array();
         var arrayNumeber = ['2', '3', '4', '5', '6', '7', '8', '9'];
-        for (let i = 0; i < 5 ; i++){
-            var caracter = arrayCards[i+8].charAt(0);
-            if(arrayNumeber.includes(caracter) ){
-                handTwo[i] = parseInt(caracter);
+        for (let i = 0; i < 5; i++) {
+            var caracter = arrayCards[i + 8].charAt(0);
+            if (arrayNumeber.includes(caracter)) {
+                handTwo[i] = {value:parseInt(caracter), suit : arrayCards[i + 8].charAt(1)};
+            } else if (caracter == 'T') {
+                handTwo[i] = {value:10, suit : arrayCards[i + 8].charAt(1)};
+            } else if (caracter == 'J') {
+                handTwo[i] = {value:11, suit : arrayCards[i + 8].charAt(1)};
+            } else if (caracter == 'Q') {
+                handTwo[i] = {value:12, suit : arrayCards[i + 8].charAt(1)};
+            } else if (caracter == 'K') {
+                handTwo[i] = {value:13, suit : arrayCards[i + 8].charAt(1)};
+            } else {
+                handTwo[i] = {value:14, suit : arrayCards[i + 8].charAt(1)};
             }
-            else if (caracter == 'T') {
-                handTwo[i] = 10;
-            }
-            else if (caracter == 'J') {
-                handTwo[i] = 11;
-            }
-            else if (caracter == 'Q') {
-                handTwo[i] = 12;
-            }
-            else if (caracter == 'K') {
-                handTwo[i] = 13;
-            }
-            else {
-                handTwo[i] = 14;
-            }
-       
         }
 
-       return handTwo
-    }  
-   
-    isMax(hand) {
-       var max =  Math.max(...hand)
-       return max
+        return handTwo;
+    }
+
+    sortHands(hand) {
+        return hand.sort((x, y) => x.value - y.value);
+    }
+
+    hightCard(hand1, hand2) {
+        var max1 = this.sortHands(hand1);
+        var max2 = this.sortHands(hand2);
+        var sizeHand = 4;
+        var win = '';
+        while (sizeHand >= 0) {
+            if (max1[sizeHand].value > max2[sizeHand].value) {
+                win = max1[sizeHand];
+                sizeHand = -1;
+            } else if (max1[sizeHand].value < max2[sizeHand].value) {
+                win = max2[sizeHand];
+                sizeHand = -1;
+            } else if (max1[sizeHand].value === max2[sizeHand].value) {
+                if (sizeHand === 0) {
+                    win = 'Tie';
+                }
+                sizeHand--;
+            }
+        }
+        return win;
+    }
+
+    searchPair(hand, newValue) {
+        var position = 0;
+        var pair = new Array();
+        hand.forEach((o, i) => {
+            if (o.value === newValue) {
+                pair[position] = i;
+                position++;
+            }
+        });
+        return pair;
+    }
+
+    pairCard(hand1) {
+        var max1 = this.sortHands(hand1);
+        //var max2 = this.sortHands(hand2);
+        var showPairs = new Array();
+        var sizeHand = 5;
+        while (sizeHand > 0) {
+            var pair = this.searchPair(hand1, max1[sizeHand - 1].value);
+            if (pair.length === 2) {
+                showPairs[0] = hand1[pair[0]];
+                showPairs[1] = hand1[pair[1]];
+            }
+            sizeHand--;
+        }
+        return  showPairs;
     }
 }
--- katas/pokerhands/maria/deck.test.js
+++ katas/pokerhands/maria/deck.test.js
@@ -6,32 +6,54 @@ describe('Tests for deck class', () => {
         const deckSplit = new Deck(cards);
         //const sizeDeck = deckSplit.dealDeck();
         expect(deckSplit.dealDeck().length).toBe(13);
-      
     });
     it('should be able to receive array and  return hand value', () => {
-        const cards = ['Black:', '2H', '3D', '5S', '9C', 'KD',' ', 'White:', '2C', '3H', '4S', '8C', 'AH'];
+        const cards = ['Black:', '2H', '3D', '5S', '9C', 'KD', ' ', 'White:', '2C', '3H', '4S', '8C', 'AH'];
         const numArray = new Deck(cards);
         expect(numArray.isHandOne(cards).length).toBe(5);
-        expect(numArray.isHandOne(cards)).toStrictEqual([2, 3, 5, 9, 13]);
-        expect(numArray.isHandTwo(cards)).toEqual([2, 3, 4, 8, 14]);
+        expect(numArray.isHandTwo(cards)).toStrictEqual([{value:2, suit:'C'}, {value:3, suit:'H'}, {value:4, suit:'S'}, {value:8, suit:'C'}, {value:14, suit:'H'}]);
+        expect(numArray.isHandOne(cards)).toEqual([{value:2, suit:'H'}, {value:3, suit:'D'}, {value:5, suit:'S'}, {value:9, suit:'C'}, {value:13, suit:'D'}]);
     });
-    it('should be able to receive array and  return max value', () => {
-        const hand1 = [2, 3, 5, 9, 13];
-        const hand2 = [2, 3, 4, 8, 14];
+    it('should be able to receive array and  return hand sort', () => {
+        const hand1 = [{value:2, suit:'H'}, {value:10, suit:'D'}, {value:1, suit:'S'}, {value:9, suit:'C'}, {value:13, suit:'H'}];
+        const hand2 = [{value:9, suit:'C'}, {value:13, suit:'H'}, {value:4, suit:'S'}, {value:1, suit:'C'}, {value:14, suit:'H'}];
+        const hand3 = [{value:9, suit:'C'}, {value:13, suit:'H'}, {value:4, suit:'S'}, {value:13, suit:'C'}, {value:14, suit:'H'}];
+        const hand4 = [{value:9, suit:'C'}, {value:1, suit:'H'}, {value:4, suit:'S'}, {value:1, suit:'C'}, {value:9, suit:'H'}];
+        const hand5 = [{value:9, suit:'C'}, {value:13, suit:'H'}, {value:9, suit:'S'}, {value:1, suit:'C'}, {value:9, suit:'H'}];
+        const hand7 = [{value:9, suit:'C'}, {value:13, suit:'H'}, {value:9, suit:'S'}, {value:9, suit:'D'}, {value:9, suit:'H'}];
+        const hand8 = [{value:9, suit:'C'}, {value:13, suit:'H'}, {value:10, suit:'S'}, {value:11, suit:'C'}, {value:12, suit:'H'}];
+        const hand9 = [{value:9, suit:'C'}, {value:7, suit:'D'}, {value:7, suit:'S'}, {value:9, suit:'D'}, {value:7, suit:'H'}];
         const numArray = new Deck();
-        expect(numArray.isMax(hand1)).toBe(13);
-        expect(numArray.isMax(hand2)).toBe(14);
-
+        expect(numArray.sortHands(hand1)).toEqual([{value:1, suit:'S'}, {value:2, suit:'H'}, {value:9, suit:'C'}, {value:10, suit:'D'}, {value:13, suit:'H'}]);
+        expect(numArray.sortHands(hand2)).toEqual([{value:1, suit:'C'}, {value:4, suit:'S'}, {value:9, suit:'C'}, {value:13, suit:'H'}, {value:14, suit:'H'}]);
+        expect(numArray.sortHands(hand3)).toEqual([{value:4, suit:'S'}, {value:9, suit:'C'}, {value:13, suit:'H'}, {value:13, suit:'C'}, {value:14, suit:'H'}]);
+        expect(numArray.sortHands(hand4)).toEqual([{value:1, suit:'H'}, {value:1, suit:'C'}, {value:4, suit:'S'}, {value:9, suit:'C'}, {value:9, suit:'H'}]);
+        expect(numArray.sortHands(hand5)).toEqual([{value:1, suit:'C'}, {value:9, suit:'C'}, {value:9, suit:'S'}, {value:9, suit:'H'}, {value:13, suit:'H'}]);
+        expect(numArray.sortHands(hand7)).toEqual([{value:9, suit:'C'}, {value:9, suit:'S'}, {value:9, suit:'D'}, {value:9, suit:'H'}, {value:13, suit:'H'}]);
+        expect(numArray.sortHands(hand8)).toEqual([{value:9, suit:'C'}, {value:10, suit:'S'}, {value:11, suit:'C'}, {value:12, suit:'H'}, {value:13, suit:'H'}]);
+        expect(numArray.sortHands(hand9)).toEqual([{value:7, suit:'D'}, {value:7, suit:'S'}, {value:7, suit:'H'}, {value:9, suit:'C'}, {value:9, suit:'D'}]);
     });
-    /*it('should be able to receive two array and  return win with high card', () => {
-        const cards = ['Black:', '2H', '3D', '5S', '9C', 'KD',' ', 'White:', '2C', '3H', '4S', '8C', 'AH'];
-        const hand1 = [2,3,5,9,13];
-        const hand2 = [2,3,4,8,14];
+    it('should be able to receive array and  return hand win in hight card value', () => {
+        const hand1 = [{value:2, suit:'H'}, {value:10, suit:'D'}, {value:1, suit:'S'}, {value:9, suit:'C'}, {value:13, suit:'D'}];
+        const hand2 = [{value:9, suit:'C'}, {value:13, suit:'H'}, {value:4, suit:'S'}, {value:1, suit:'C'}, {value:14, suit:'H'}];
+        const hand3 = [{value:9, suit:'C'}, {value:13, suit:'H'}, {value:4, suit:'S'}, {value:1, suit:'C'}, {value:11, suit:'H'}];
+        const hand4 = [{value:7, suit:'C'}, {value:13, suit:'D'}, {value:4, suit:'S'}, {value:1, suit:'C'}, {value:14, suit:'S'}];
+        const hand5 = [{value:9, suit:'C'}, {value:13, suit:'D'}, {value:6, suit:'S'}, {value:1, suit:'C'}, {value:14, suit:'S'}];
+        const hand6 = [{value:9, suit:'C'}, {value:13, suit:'D'}, {value:4, suit:'S'}, {value:2, suit:'C'}, {value:14, suit:'S'}];
+        const hand7 = [{value:2, suit:'C'}, {value:3, suit:'D'}, {value:5, suit:'S'}, {value:8, suit:'C'}, {value:13, suit:'S'}];
+        const hand8 = [{value:2, suit:'D'}, {value:3, suit:'D'}, {value:5, suit:'H'}, {value:8, suit:'5'}, {value:13, suit:'S'}];
         const numArray = new Deck();
-        expect(numArray.isMax(hand1)).toBe('AH');
-        //xpect(numArray.isMax(hand2)).toBe(14);
-
-    });*/
-
-    
+        expect(numArray.hightCard(hand1, hand2)).toEqual({value:14, suit:'H'});
+        expect(numArray.hightCard(hand1, hand3)).toEqual({value:11, suit:'H'});
+        expect(numArray.hightCard(hand2, hand4)).toEqual({value:9, suit:'C'});
+        expect(numArray.hightCard(hand2, hand5)).toEqual({value:6, suit:'S'});
+        expect(numArray.hightCard(hand2, hand6)).toEqual({value:2, suit:'C'});
+        expect(numArray.hightCard(hand7, hand8)).toEqual('Tie');
+    });
+    it('should be able to receive array and  return hand win in pair card value', () => {
+        const hand1 = [{value:2, suit:'H'}, {value:10, suit:'D'}, {value:1, suit:'S'}, {value:2, suit:'C'}, {value:13, suit:'D'}];
+        //const hand2 = [{value:7, suit:'C'}, {value:13, suit:'D'}, {value:4, suit:'S'}, {value:1, suit:'C'}, {value:14, suit:'S'}];
+        const numArray = new Deck();
+        expect(numArray.pairCard(hand1)).toEqual([{value:2, suit:'H'}, {value:2, suit:'C'}]);
+    });
 });
