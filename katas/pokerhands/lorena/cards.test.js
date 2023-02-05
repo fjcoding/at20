dbb58c6fd0a0872d2775f5cefa7cb7157e752ffd
Lorena Ramirez <Lorena.Ramirez@jala.university>
2023-02-05 14:52:11 -0500

    Add changes in card.test file

--- katas/pokerhands/lorena/cards.test.js
+++ katas/pokerhands/lorena/cards.test.js
@@ -1,6 +1,5 @@
 import { Card } from './cards';
 
-
 describe('Test for Card class', () => {
     it('Should be able to identify a specific card', () => {
         const value = '7';
@@ -10,15 +9,15 @@ describe('Test for Card class', () => {
     });
 
     it('should be able identify card`s value', () => {
-        const value = '7';
-        const suite = 'D';
+        const value = '2';
+        const suite = 'H';
         const card = new Card(value, suite);
         expect(card.cardREad()[0]).toBe(value);
     });
 
     it('should be able identify card`s suite', () => {
-        const value = '7';
-        const suite = 'D';
+        const value = 'A';
+        const suite = 'S';
         const card = new Card(value, suite);
         expect(card.cardREad()[1]).toBe(suite);
     });
