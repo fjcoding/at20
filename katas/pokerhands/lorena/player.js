5828b4fa8cdfa7df92c4dee74057eb4b4d6625ec
Lorena Ramirez <Lorena.Ramirez@jala.university>
2023-02-05 17:14:50 -0500

    Add unit test of pokerhand's suits and values

--- katas/pokerhands/lorena/player.js
+++ katas/pokerhands/lorena/player.js
@@ -17,30 +17,46 @@ export class Player {
         return this.pokerhand.split(' ');
     }
 
-    valuesInHand () {
+    switchValInHand () {
         var handArray = this.pokerhand.split (' ');
-        var handArrayVal = [];
+        var handWhitVal = [];
 
         for (let i = 0; i <= 4; i++) {
             var actCard = handArray[i];
             actCard = Array.from(actCard);
-            console.log(actCard);
             if (actCard[0] == 'J') {
                 actCard[0] = 11;
-            }
-            else if (actCard[0] == 'Q') {
+            } else if (actCard[0] == 'Q') {
                 actCard[0] = 12;
-            }
-            else if (actCard[0] == 'K') {
+            } else if (actCard[0] == 'K') {
                 actCard[0] = 13;
-            }
-            else if (actCard[0] == 'A') {
+            } else if (actCard[0] == 'A') {
                 actCard[0] = 14;
             }
             actCard = actCard[0] + actCard[1];
-            handArrayVal.push(actCard);
-            console.log (handArrayVal);
+            handWhitVal.push(actCard);
         }
-        return handArrayVal;
+        return handWhitVal;
+    }
+
+    valuesHand() {
+        var hand = this.switchValInHand();
+        var valuesHand = [];
+        for (let i = 0; i <= 4; i++) {
+            var actCard = hand[i];
+            valuesHand[i] = parseInt(actCard);
+        }
+        return valuesHand;
+    }
+
+    suitsHand() {
+        var hand = this.switchValInHand();
+        var suitsHand = [];
+        for (let i = 0; i <= 4; i++) {
+            var actCard = hand[i];
+            actCard = Array.from(actCard);
+            suitsHand[i] = actCard[actCard.length - 1];
+        }
+        return suitsHand;
     }
 }
