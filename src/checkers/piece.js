0e5301547d9e137c602df73f6e462730636b3676
Williams Velasquez <Williams.Velasquez@jala.university>
2023-02-07 19:07:49 -0400

    Add file and class piece

---
+++ src/checkers/piece.js
@@ -0,0 +1,21 @@
+class Piece {
+  constructor(color, x, y) {
+    this.color = color;
+    this.x = x;
+    this.y = y;
+    this.queen = false;
+  }
+
+  move(newX, newY) {
+    this.x = newX;
+    this.y = newY;
+  }
+
+  isQueen() {
+    return this.queen;
+  }
+
+  makeQueen() {
+    this.queen = true;
+  }
+}
