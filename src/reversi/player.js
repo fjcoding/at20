3eb65d93b5ff76856588854469610f5f9bd9efa3
Bryan Colque Clavijo <Bryan.Colque@jala.university>
2023-02-07 18:06:26 -0400

    Lint changes added

--- src/reversi/player.js
+++ src/reversi/player.js
@@ -2,19 +2,19 @@
 export class Player {
     // #posX
     // #posY
-    #playerTag
-     constructor(playerTag) {
-         this.#playerTag=playerTag; 
-         //this.posX = posX;
-         //this.posY = posY;
-         this.tokenCount = 28;    }
-     setToken(posX,posY) {
-         this.tokenCount-=1;
-         return [posX,posY,this.tokenCount, this.#playerTag];
-        
-     }
-     
- }
- 
+    #playerTag;
+
+    constructor(playerTag) {
+        this.#playerTag = playerTag;
+        //this.posX = posX;
+        //this.posY = posY;
+        this.tokenCount = 28;
+    }
+
+    setToken(posX, posY) {
+        this.tokenCount -= 1;
+        return [posX, posY, this.tokenCount, this.#playerTag];
+    }
+}
 
 
