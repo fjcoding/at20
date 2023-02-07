95e0f82bd4b6f51a23bd7e53aca5a6946bcd3f7b
Percy Fernandez <Percy.Fernandez@jala.university>
2023-02-07 18:28:29 -0400

    lint fix

--- src/reversi/positionOnBoard.js
+++ src/reversi/positionOnBoard.js
@@ -1,29 +1,29 @@
 
 export class positionOnBoard {
-     #posX
-     #posY
+    #posX;
 
-     #color
-    #board=[]
-     constructor(board,color) {
-         this.#board=board;
-         this.#color=color;
+    #posY;
 
+    #color;
 
-        }
+    #board = [];
 
-     asPosition() {
-        var arr=[];
-        const a=this.#board.length;
-       for (var i = 0; i < a; i++) {
+    constructor(board, color) {
+        this.#board = board;
+        this.#color = color;
+    }
+
+    asPosition() {
+        var arr = [];
+        const a = this.#board.length;
+        for (var i = 0; i < a; i++) {
             for (var j = 0; j < a; j++) {
-            if (this.#board[i][j]==this.#color) {
-               arr.push([i,j])
-          }
+                if (this.#board[i][j] == this.#color) {
+                    arr.push([i, j]);
+                }
+            }
         }
-       }
         return arr;
-     }
-
- }
+    }
+}
 
