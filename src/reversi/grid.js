a71a1b6a2d3dcd239e094b37a5044a3d406293ff
Lorena Ramirez <Lorena.Ramirez@jala.university>
2023-02-07 17:50:36 -0500

    Fix class Grid lint issues

--- src/reversi/grid.js
+++ src/reversi/grid.js
@@ -3,7 +3,7 @@ export class Grid {
 
     #posY;
 
-    #color
+    #color;
 
     #map;
 
@@ -20,7 +20,7 @@ export class Grid {
     gridInit () {
         const mapDim = 8;
         this.#map = new Array(mapDim).fill().map(() => Array(mapDim).fill(' '));
-        this.#map[3][3] = this.#white;
+        this.#map[this.#posX][this.#posY] = this.#color;
         this.#map[4][3] = this.#black;
         this.#map[3][4] = this.#black;
         this.#map[4][4] = this.#white;
