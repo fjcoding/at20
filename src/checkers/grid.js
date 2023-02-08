ff9d5557189715cff410d48ce242102de560e7df
Carlos CRodriguez <Carlos.Caceres@jala.university>
2023-02-08 19:41:32 -0400

    Add grid.js upgrade and conversion to class with Williams

--- src/checkers/grid.js
+++ src/checkers/grid.js
@@ -1,52 +1,33 @@
 export class grid {
-    WIDTH = 800;
-    HEIGHT = 800;
-
-    COLUMNS = 8;
-    ROWS = 8;
-
-    CELL_WIDTH = WIDTH / COLUMNS;
-    CELL_HEIGHT = HEIGHT / ROWS;
-
-    theme = {
-        light : '#eeeed2',
-        dark : '#5F9EA0',
-    };
-
-    constructor ($canvas, context){
-        $canvas = document.createElement('canvas');
-        context = $canvas.getContext('2d');
-
-        $canvas.width = WIDTH;
-        $canvas.height = HEIGHT;
-
-        context.textBaseline = 'middle';
-        context.textAlign = 'center';
-
-        document.body.appendChild($canvas);
+    #gridBoard;
+    constructor () {
+        this.#gridBoard = [];
+        for (let i = 0; i < 8; i++) {
+            this.#gridBoard[i] = [];
+            for (let j = 0; j < 8; j++) {
+                this.#gridBoard [i][j] = null;                
+            }            
+        }
     }
-    
-    startGrid () {
-        const boardMatrix = [];
-        for (let ejeX = 0; ejeX < COLUMNS; ejex += 1) {
-            boardMatrix[ejeX] = [];
-            for (let ejeY = 0; ejeY < ROWS; ejeY += 1) {
-                boardMatrix[ejeX][ejeY] = null;
 
-                let rectColor = theme.light;
-                let textColor = theme.dark;
-
-                if ((ejeX + ejeY) % 2) {
-                    rectColor = theme.dark;
-                    textColor = theme.light;
+    showGrid () {
+        console.log('  A B C D E F G H');
+        console.log('------------------');
+        for (let i = 0; i < 8; i++) {
+            let row = `${i}|`;
+            for (let j = 0; j < 8; j++) {
+                if ((i + j) % 2 === 0) {
+                    row += '■|';
                 }
-
-                context.fillStyle = rectColor;
-                context.fillRect(ejeX * CELL_WIDTH, ejeY * CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT);
-
-                context.fillStyle = textColor;
-                context.fillText(`[${ejeX}; ${ejeY}]`, ejeX * CELL_WIDTH + CELL_WIDTH / 2, ejeY * CELL_HEIGHT + CELL_HEIGHT / 2);
+                else if (this.#gridBoard[i][j] === null) {
+                    row += ' |';
+                } 
             }
+            console.log(row);           
         }
     }
 }
+
+let grip = new grid();
+grip.showGrid();
+console.log('------------------');
