09c7bdef81949a54b301d01d7c8087aaa2993da6
Jose Guevara <Jose.Guevara@Jala.University>
2023-02-08 09:13:10 -0400

    Add CardValue with numeric and symbol properties

--- katas/pokerhands/jose/card.js
+++ katas/pokerhands/jose/card.js
@@ -16,3 +16,19 @@ export class Card {
         return this.#value;
     }
 }
+
+export const CardValue = {
+    Two: { numeric: 2, symbol: '2' },
+    Three: { numeric: 3, symbol: '3' },
+    Four: { numeric: 4, symbol: '4' },
+    Five: { numeric: 5, symbol: '5' },
+    Six: { numeric: 6, symbol: '6' },
+    Seven: { numeric: 7, symbol: '7' },
+    Eight: { numeric: 8, symbol: '8' },
+    Nine: { numeric: 9, symbol: '9' },
+    Ten: { numeric: 10, symbol: 'T' },
+    Jack: { numeric: 11, symbol: 'J' },
+    Queen: { numeric: 12, symbol: 'Q' },
+    King: { numeric: 13, symbol: 'K' },
+    Ace: { numeric: 14, symbol: 'A' }
+};
--- katas/pokerhands/jose/card.test.js
+++ katas/pokerhands/jose/card.test.js
@@ -1,9 +1,15 @@
-import { Card } from './card';
+import { Card, CardValue } from './card';
 
 describe('Tests for Card class', () => {
     it('should have a suit and value and be able to return those values', () => {
-        const card = new Card('C', 2);
+        const card = new Card('C', CardValue.Two);
         expect(card.suit).toBe('C');
-        expect(card.value).toBe(2);
+        expect(card.value.numeric).toBe(2);
+        expect(card.value.symbol).toBe('2');
+
+        const aceSpades = new Card('S', CardValue.Ace);
+        expect(aceSpades.suit).toBe('S');
+        expect(aceSpades.value.numeric).toBe(14);
+        expect(aceSpades.value.symbol).toBe('A');
     });
 });
