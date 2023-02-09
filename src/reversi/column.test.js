96a5384ead538cf6f1a48ad7dc31116b529e37b6
Giovanni Blanco <Giovanni.Blanco@jala.university>
2023-02-09 15:17:21 -0400

    Add a white test case in column.test

--- src/reversi/column.test.js
+++ src/reversi/column.test.js
@@ -19,3 +19,22 @@ test('Check for column white pieces between two black ', () => {
     const newCheck = new Column(newPosition, testMap, black.playerTag);
     expect(newCheck.checkColumn(piecesPosition)).toStrictEqual(whitePiecesCord);//
 });
+
+test('Check for column white pieces between two white ', () => {
+    const black = new Player('W');
+    const newPosition = black.setToken(1, 4);// [posx,posy,tokens left,palyerTag]
+    const testMap = [
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', 'W', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', 'B', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', 'B', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', 'B', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', 'B', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', 'W', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', 'B', ' ', ' ', ' ']
+    ];
+    const piecesPosition = [];
+    const blackPiecesCord = [[2, 4], [3, 4], [4, 4], [5, 4]];
+    const newCheck = new Column(newPosition, testMap, black.playerTag);
+    expect(newCheck.checkColumn(piecesPosition)).toStrictEqual(blackPiecesCord);//
+});
