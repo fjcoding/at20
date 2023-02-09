a745e1a5b3a90bb67496befd6c265602d3301a39
Carlos CRodriguez <Carlos.Caceres@jala.university>
2023-02-08 21:48:28 -0400

    Add grid.js upgrade and conversion to class with Williams

--- src/checkers/grid.js
+++ src/checkers/grid.js
@@ -1,14 +1,16 @@
 export class grid {
     #gridBoard;
+
     constructor () {
         this.#gridBoard = [];
         for (let i = 0; i < 8; i++) {
             this.#gridBoard[i] = [];
             for (let j = 0; j < 8; j++) {
-                this.#gridBoard [i][j] = null;                
-            }            
+                this.#gridBoard [i][j] = null;
+            }
         }
     }
+
     showGrid () {
         console.log('  A B C D E F G H');
         console.log('------------------');
@@ -17,12 +19,11 @@ export class grid {
             for (let j = 0; j < 8; j++) {
                 if ((i + j) % 2 === 0) {
                     row += '■|';
-                }
-                else if (this.#gridBoard[i][j] === null) {
+                } else if (this.#gridBoard[i][j] === null) {
                     row += ' |';
-                } 
+                }
             }
-            console.log(row);           
+            console.log(row);
         }
     }
 }
