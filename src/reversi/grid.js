cea2278ae2960a3bd1efff1e943c92f3215d271f
Lorena Ramirez <Lorena.Ramirez@jala.university>
2023-02-09 15:14:44 -0500

    Add method for update the board

--- src/reversi/grid.js
+++ src/reversi/grid.js
@@ -1,9 +1,9 @@
 export class Grid {
-    #posX;
+    // #posX;
 
-    #posY;
+    // #posY;
 
-    #color;
+    // #color;
 
     #map;
 
@@ -11,20 +11,25 @@ export class Grid {
 
     #white = 'w';
 
-    constructor(posX, posY, color) {
-        this.#posX = posX;
-        this.#posY = posY;
-        this.#color = color;
-    }
+    // constructor(posX, posY, color) {
+    //     this.#posX = posX;
+    //     this.#posY = posY;
+    //     this.#color = color;
+    // }
 
     gridInit () {
         const mapDim = 8;
         this.#map = new Array(mapDim).fill().map(() => Array(mapDim).fill(' '));
-        this.#map[this.#posX][this.#posY] = this.#color;
+        this.#map[3][3] = this.#white;
         this.#map[4][3] = this.#black;
         this.#map[3][4] = this.#black;
         this.#map[4][4] = this.#white;
 
         return this.#map;
+    }
+
+    updateBoard (board) {
+        let actBoard = board;
+        return actBoard;
     }
 }
