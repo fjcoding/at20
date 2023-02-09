ea5a2e3abeba05c8e3aff228c799ce5b6316e15b
Adalid Perez <Adalid.Perez@jala.university>
2023-02-08 21:00:42 -0400

    Add changes in the countPiece.test.js file

--- src/checkers/countPiece.test.js
+++ src/checkers/countPiece.test.js
@@ -7,11 +7,4 @@ describe('Tests for CountPiece class', () => {
         const countPiece = new CountPiece(totalPieces, lostPieces);
         expect(countPiece.getTotalPieces()).toBe(9);
     });
-
-    it('should be able to return the correct amount pieces', () => {
-        let totalPieces = 12;
-        let lostPieces = 3;
-        const countPiece = new CountPiece(totalPieces, lostPieces);
-        expect(countPiece.getTotalPieces()).toBe(9);
-    });
 });
