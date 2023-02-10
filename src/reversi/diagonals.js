6c2d4317bf899b10993f6e7d9c8a57b10e3d85e5
Bryan Colque Clavijo <Bryan.Colque@jala.university>
2023-02-10 18:18:16 -0400

    A few changes added to shorten the code

--- src/reversi/diagonals.js
+++ src/reversi/diagonals.js
@@ -23,135 +23,76 @@ export class Diagonals {
         var sinY;
         var step = 1;
         var steps = 0;
-        if (ntValue == 'B') {
-            for (var signX = 0; signX < 2; signX++) {// 11  00 77  14 26 41 63
-                for (var signY = 0; signY < 2; signY++) {
-                    sinX = (sign ** signX);
-                    sinY = (sign ** signY);
-                    traDiags = [];
 
-                    if (sinX + sinY == 2) {
-                        var max = Math.max(...this.#newTokenpos);
-                        steps = 7 - max;
-                        step = 1;
-                    } else if (sinX + sinY == -2) {
-                        var min = Math.min(...this.#newTokenpos);
-                        steps = min;
-                        step = 1;
-                    } else if (sinX > sinY && ntxCoor >= ntyCoor && ntxCoor + ntyCoor <= 7) {
-                        min = Math.min(...this.#newTokenpos);
-                        steps = min;
-                        step = 1;
-                    } else if (sinX > sinY && ntxCoor < ntyCoor && ntxCoor + ntyCoor <= 7) {
-                        max = Math.max(...this.#newTokenpos);
-                        steps = max;
-                        step = 1;
-                    } else if (sinX > sinY && ntxCoor >= ntyCoor && ntxCoor + ntyCoor > 7) { //----------------
-                        max = Math.max(...this.#newTokenpos);
-                        steps = 7 - max;
-                        step = 1;
-                    } else if (sinX > sinY && ntxCoor < ntyCoor && ntxCoor + ntyCoor > 7) {
-                        min = Math.min(...this.#newTokenpos);
-                        steps = 7 - min;
-                        step = 1;
-                    } else if (sinX < sinY && ntxCoor >= ntyCoor && ntxCoor + ntyCoor <= 7) {
-                        max = Math.max(...this.#newTokenpos);
-                        steps = max;
-                        step = 1;
-                    } else if (sinX < sinY && ntxCoor < ntyCoor && ntxCoor + ntyCoor <= 7) {
-                        min = Math.min(...this.#newTokenpos);
-                        steps = min;
-                        step = 1;
-                    } else if (sinX < sinY && ntxCoor >= ntyCoor && ntxCoor + ntyCoor > 7) { //----------------
-                        min = Math.min(...this.#newTokenpos);
-                        steps = 7 - min;
-                        step = 1;
-                    } else if (sinX < sinY && ntxCoor < ntyCoor && ntxCoor + ntyCoor > 7) {
-                        max = Math.max(...this.#newTokenpos);
-                        steps = 7 - max;
-                        step = 1;
-                    }
+        if (ntValue == 'W') {
+            var opValue = 'B';
+        } else if (ntValue == 'B') {
+            opValue = 'W';
+        }
+        for (var signX = 0; signX < 2; signX++) {
+            for (var signY = 0; signY < 2; signY++) {
+                sinX = (sign ** signX);
+                sinY = (sign ** signY);
+                traDiags = [];
 
-                    while (step <= steps) {
-                        if (this.#grid[ntxCoor + sinX * step][ntyCoor + sinY * step] == 'W') {
-                            traDiags.push([ntxCoor + sinX * step, ntyCoor + sinY * step]);
-                        } else if (this.#grid[ntxCoor + sinX * step][ntyCoor + sinY * step] == ntValue) {
-                            newDiags = newDiags.concat(traDiags);
-                            traDiags = [];
-                            break;
-                        } else if (this.#grid[ntxCoor + sinX * step][ntyCoor + sinY * step] == ' ') {
-                            traDiags = [];
-                            break;
-                        }
-                        step += 1;
-                    }
+                if (sinX + sinY == 2) {
+                    var max = Math.max(...this.#newTokenpos);
+                    steps = 7 - max;
+                    step = 1;
+                } else if (sinX + sinY == -2) {
+                    var min = Math.min(...this.#newTokenpos);
+                    steps = min;
+                    step = 1;
+                } else if (sinX > sinY && ntxCoor >= ntyCoor && ntxCoor + ntyCoor <= 7) {
+                    min = Math.min(...this.#newTokenpos);
+                    steps = min;
+                    step = 1;
+                } else if (sinX > sinY && ntxCoor < ntyCoor && ntxCoor + ntyCoor <= 7) {
+                    max = Math.max(...this.#newTokenpos);
+                    steps = max;
+                    step = 1;
+                } else if (sinX > sinY && ntxCoor >= ntyCoor && ntxCoor + ntyCoor > 7) { //----------------
+                    max = Math.max(...this.#newTokenpos);
+                    steps = 7 - max;
+                    step = 1;
+                } else if (sinX > sinY && ntxCoor < ntyCoor && ntxCoor + ntyCoor > 7) {
+                    min = Math.min(...this.#newTokenpos);
+                    steps = 7 - min;
+                    step = 1;
+                } else if (sinX < sinY && ntxCoor >= ntyCoor && ntxCoor + ntyCoor <= 7) {
+                    max = Math.max(...this.#newTokenpos);
+                    steps = max;
+                    step = 1;
+                } else if (sinX < sinY && ntxCoor < ntyCoor && ntxCoor + ntyCoor <= 7) {
+                    min = Math.min(...this.#newTokenpos);
+                    steps = min;
+                    step = 1;
+                } else if (sinX < sinY && ntxCoor >= ntyCoor && ntxCoor + ntyCoor > 7) { //----------------
+                    min = Math.min(...this.#newTokenpos);
+                    steps = 7 - min;
+                    step = 1;
+                } else if (sinX < sinY && ntxCoor < ntyCoor && ntxCoor + ntyCoor > 7) {
+                    max = Math.max(...this.#newTokenpos);
+                    steps = 7 - max;
+                    step = 1;
                 }
-            }
-        } else if (ntValue == 'W') {
-            for (signX = 0; signX < 2; signX++) {
-                for (signY = 0; signY < 2; signY++) {
-                    sinX = (sign ** signX);
-                    sinY = (sign ** signY);
-                    traDiags = [];
 
-
-                    if (sinX + sinY == 2) {
-                        max = Math.max(...this.#newTokenpos);
-                        steps = 7 - max;
-                        step = 1;
-                    } else if (sinX + sinY == -2) {
-                        min = Math.min(...this.#newTokenpos);
-                        steps = min;
-                        step = 1;
-                    } else if (sinX > sinY && ntxCoor >= ntyCoor && ntxCoor + ntyCoor <= 7) {
-                        min = Math.min(...this.#newTokenpos);
-                        steps = min;
-                        step = 1;
-                    } else if (sinX > sinY && ntxCoor < ntyCoor && ntxCoor + ntyCoor <= 7) {
-                        max = Math.max(...this.#newTokenpos);
-                        steps = max;
-                        step = 1;
-                    } else if (sinX > sinY && ntxCoor >= ntyCoor && ntxCoor + ntyCoor > 7) { //----------------
-                        max = Math.max(...this.#newTokenpos);
-                        steps = 7 - max;
-                        step = 1;
-                    } else if (sinX > sinY && ntxCoor < ntyCoor && ntxCoor + ntyCoor > 7) {
-                        min = Math.min(...this.#newTokenpos);
-                        steps = 7 - min;
-                        step = 1;
-                    } else if (sinX < sinY && ntxCoor >= ntyCoor && ntxCoor + ntyCoor <= 7) {
-                        max = Math.max(...this.#newTokenpos);
-                        steps = max;
-                        step = 1;
-                    } else if (sinX < sinY && ntxCoor < ntyCoor && ntxCoor + ntyCoor <= 7) {
-                        min = Math.min(...this.#newTokenpos);
-                        steps = min;
-                        step = 1;
-                    } else if (sinX < sinY && ntxCoor >= ntyCoor && ntxCoor + ntyCoor > 7) { //----------------
-                        min = Math.min(...this.#newTokenpos);
-                        steps = 7 - min;
-                        step = 1;
-                    } else if (sinX < sinY && ntxCoor < ntyCoor && ntxCoor + ntyCoor > 7) {
-                        max = Math.max(...this.#newTokenpos);
-                        steps = 7 - max;
-                        step = 1;
+                while (step <= steps) {
+                    if (this.#grid[ntxCoor + sinX * step][ntyCoor + sinY * step] == opValue) {
+                        traDiags.push([ntxCoor + sinX * step, ntyCoor + sinY * step]);
+                    } else if (this.#grid[ntxCoor + sinX * step][ntyCoor + sinY * step] == ntValue) {
+                        newDiags = newDiags.concat(traDiags);
+                        traDiags = [];
+                        break;
+                    } else if (this.#grid[ntxCoor + sinX * step][ntyCoor + sinY * step] == ' ') {
+                        traDiags = [];
+                        break;
                     }
-                    while (step <= steps) {
-                        if (this.#grid[ntxCoor + sinX * step][ntyCoor + sinY * step] == 'B') {
-                            traDiags.push([ntxCoor + sinX * step, ntyCoor + sinY * step]);
-                        } else if (this.#grid[ntxCoor + sinX * step][ntyCoor + sinY * step] == ntValue) {
-                            newDiags = newDiags.concat(traDiags);
-                            traDiags = [];
-                            break;
-                        } else if (this.#grid[ntxCoor + sinX * step][ntyCoor + sinY * step] == ' ') {
-                            traDiags = [];
-                            break;
-                        }
-                        step += 1;
-                    }
+                    step += 1;
                 }
             }
         }
+
         return newDiags;
     }
 }
