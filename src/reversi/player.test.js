3250c970b493eb4247e6b5ac562a104a709f45a2
Percy Fernandez <Percy.Fernandez@jala.university>
2023-02-10 08:12:25 -0400

    fixing issues

--- src/reversi/player.test.js
+++ src/reversi/player.test.js
@@ -1,11 +1,4 @@
 import { Player } from './player';
-test('Create a player and set a token on the board', () => {
-    const whitePlayer = new Player('W');
-    const blackPlayer = new Player('B');
-    expect(whitePlayer.setToken(1, 1)).toStrictEqual([1, 1]);
-    expect(blackPlayer.setToken(6, 1)).toStrictEqual([6, 1]);
-});
-
 
 test('Create a player and check the players tag', () => {
     const whitePlayer = new Player('W');
