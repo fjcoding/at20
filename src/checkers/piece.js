f1ff1c90350fabb20bdc943b368cde049005c0d7
Williams Velasquez <Williams.Velasquez@jala.university>
2023-02-09 21:06:38 -0400

    Refactoring and implement encapsulation class Piece and modify piece.test

--- src/checkers/piece.js
+++ src/checkers/piece.js
@@ -1,21 +1,38 @@
 export class Piece {
+
+    #color;
+    #x;
+    #y;
+    #typeChain; 
+
     constructor(color, x, y) {
-        this.color = color;
-        this.x = x;
-        this.y = y;
-        this.typeChain = 'pawn';
+        this.#color = color;
+        this.#x = x;
+        this.#y = y;
+        this.#typeChain = 'pawn';
     }
 
+    getX(){
+        return this.#x;
+    }
+
+    getY(){
+        return this.#y;
+    }
+
+    getColor(){
+        return this.#color;
+    }
     currentlyPosition(newX, newY) {
-        this.x = newX;
-        this.y = newY;
+        this.#x = newX;
+        this.#y = newY;
     }
 
     getTypeChain() {
-        return this.typeChain;
+        return this.#typeChain;
     }
 
     makeLady() {
-        this.typeChain = 'lady';
+        this.#typeChain = 'lady';
     }
 }
