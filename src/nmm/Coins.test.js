7885a5098ff49406b5b54c50442e91fd1c86bd72
Daniel Carrasco <Daniel.Carrasco@jala.university>
2023-02-10 17:28:22 -0400

    Add new coin class for the NMM project

---
+++ src/nmm/Coins.test.js
@@ -0,0 +1,13 @@
+import { Coins } from './coins';
+
+describe ('Test to know about a place with a *', () =>{
+    it('should set the symbol and state correctly', () => {
+        const coin = new Coins('*');
+        expect(coin.symbol).toBe('*');
+        expect(coin.state).toBe('active');
+
+        const coin2 = new Coins('-');
+        expect(coin2.symbol).toBe('-');
+        expect(coin2.state).toBe('inactive');
+      });
+});
