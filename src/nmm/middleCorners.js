88b499eeace0999d6609e0205a6ea891f655e20c
Israel Zambrana Galvan <Israel.Zambrana@jala.university>
2023-02-11 16:16:24 -0400

    Added middleCorners.js and middleCorners.test.js to check moving middle corners

---
+++ src/nmm/middleCorners.js
@@ -0,0 +1,35 @@
+import { positions } from './possiblePositions';
+export class middleCorners {
+    #middlePosition;
+
+    constructor() {
+        this.#middlePosition = {
+            coner1 : positions.b5,
+            coner2 : positions.f5,
+            coner3 : positions.b1,
+            coner4 : positions.f1
+        };
+    }
+
+    ismiddle(position) {
+        for (let coner in this.#middlePosition) {
+            const row = this.#middlePosition[coner].row;
+            const col = this.#middlePosition[coner].col;
+            if (row === position[0] && col === position[1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    checkMove(currentPosition, newPosition) {
+        const spaceRow = Math.abs(currentPosition[0] - newPosition[0]);
+        const spaceCol = Math.abs(currentPosition[1] - newPosition[1]);
+        if (spaceRow === 2 ^ spaceCol === 2) {
+            if (this.ismiddle(currentPosition)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
