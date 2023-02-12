dc86c0e906fdfb29cd1bcd54ce5128a934d835dd
Percy Fernandez <Percy.Fernandez@jala.university>
2023-02-12 11:26:07 -0400

    changes

---
+++ src/reversi/player.js
@@ -0,0 +1,23 @@
+export class Player {
+    #playerTag;
+
+    #tokenCount;
+
+    constructor(playerTag) {
+        this.#playerTag = playerTag;
+        this.#tokenCount = 28;
+    }
+
+    setToken(posX, posY) {
+        this.#tokenCount -= 1;
+        return [posX, posY];
+    }
+
+    get tokenCount() {
+        return this.#tokenCount;
+    }
+
+    get playerTag() {
+        return this.#playerTag;
+    }
+}
