2c3d569c7f62ea0dd89b30147cc62f7de90379f4
Daniel Carrasco <Daniel.Carrasco@jala.university>
2023-02-14 00:52:37 -0400

    check if there are available positions in the grid

--- src/nmm/grid.test.js
+++ src/nmm/grid.test.js
@@ -14,5 +14,22 @@ describe('Tests for Grid class', () => {
         const gridInit = new Grid();
         expect(gridInit.showGridInit()).toEqual(grid);
     });
+
+    it('Should be able to return true if there are available positions in the grid', () =>{
+        const grid = new Grid();
+        expect (grid.checkAvailablePos()).toBe(true);
+    });
+
+    it('Should be able to return false if there are no available positions in the grid', () =>{
+        const grid = new Grid();
+        if (grid.gridInit && grid.gridInit.length) {
+            for (let row = 0; row < grid.gridInit.length; row++) {
+                for (let col = 0; col < grid.gridInit.length; col++) {
+                    grid.gridInit[row][col].symbol = Grid.coinSymbols.PA;
+                }
+            }
+        }
+        expect(grid.checkAvailablePos()).toBe(false);
+    });
 });
 
