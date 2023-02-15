002350be0ec1c14f70e5d6767a263383d3b728f6
Adalid Perez <Adalid.Perez@jala.university>
2023-02-15 08:01:42 -0400

    Continue doing the interface with the game

--- src/checkers/main.js
+++ src/checkers/main.js
@@ -16,7 +16,7 @@ const askAndRead = (text) => new Promise(resolve => {
 );
 
 const main = async () => {
-    let name;
+    let name1, name2;
     let color;
     let posX, posY, x1, y1;
     const game = new Game();
@@ -24,28 +24,42 @@ const main = async () => {
     console.log('WELCOME TO CHECKERS GAME');
     console.log('========================================');
 
-    name = await askAndRead('Introduce a name => ');
-
+    console.log('FIRST PLAYER');
+    name1 = await askAndRead('Introduce a name for the FIRST player => ');
     color = await askAndRead('Introduce the color of the piece( W -> WHITE, R -> RED)=> ');
+    const player1 = new Player(name1, color, 12, game);
+
+    console.log('SECOND PLAYER');
+    name2 = await askAndRead('Introduce a name for the SECOND player => ');
+    color = await askAndRead('Introduce the color of the piece( W -> WHITE, R -> RED)=> ');
+    const player2 = new Player(name2, color, 12, game);
+
     console.log(game.showBoard());
+
+    passTurn(player1, player2);
+
 
     posX = await askAndRead('Introduce the position select of the piece in the X  => ');
     x1 = parseInt(posX);
-
     posY = await askAndRead('Introduce the position select of the piece in the Y  => ');
     y1 = parseInt(posY);
-
-    const player1 = new Player(name, color, 12, game);
     player1.selectPiece({ x: x1, y: y1 });
 
     posX = await askAndRead('Introduce the NEW position for move the piece in  X  => ');
     x1 = parseInt(posX);
     posY = await askAndRead('Introduce the NEW position for move the piece in  Y  => ');
     y1 = parseInt(posY);
-
     player1.movePiece({ x: x1, y: y1 });
+
     game.refreshGrid();
     console.log(game.showBoard());
+};
+
+const passTurn = async (player1, player2) => {
+    // let moveDone = false;
+    // let posX, posY, x1, y1;
+    while (player1.pieces > 0 || player2.pieces > 0) {
+    }
 };
 main();
 
