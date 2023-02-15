ac7f07267ee6e4e1a26d4d7ed2643335d953a85f
Adalid Perez <Adalid.Perez@jala.university>
2023-02-14 22:55:14 -0400

    Add the first step for an interface for the game

--- src/checkers/main.js
+++ src/checkers/main.js
@@ -1,20 +1,52 @@
 import { Player } from './player.js';
 import { Game } from './game.js';
+import * as ReadLine from 'readline';
+import { stdin as input, stdout as output } from 'node:process';
 
-const game = new Game();
-console.log(game.showBoard());
+console.clear();
 
-const player1 = new Player('Babe', 'R', 12, game);
-player1.selectPiece({ x: 2, y: 0 });
-player1.movePiece({ x: 3, y: 1 });
+const askAndRead = (text) => new Promise(resolve => {
+    const readLine = ReadLine.createInterface({ input, output });
 
-game.refreshGrid();
-console.log(game.showBoard());
+    readLine.question(text, (data) => {
+        readLine.close();
+        resolve(data);
+    });
+}
+);
 
-const player2 = new Player('Jude', 'W', 12, game);
-player2.selectPiece({ x: 5, y: 7 });
-player2.movePiece({ x: 4, y: 6 });
-game.refreshGrid();
-console.log(game.showBoard());
+const main = async () => {
+    let name;
+    let color;
+    let posX, posY, x1, y1;
+    const game = new Game();
+
+    console.log('WELCOME TO CHECKERS GAME');
+    console.log('========================================');
+
+    name = await askAndRead('Introduce a name => ');
+
+    color = await askAndRead('Introduce the color of the piece( W -> WHITE, R -> RED)=> ');
+    console.log(game.showBoard());
+
+    posX = await askAndRead('Introduce the position select of the piece in the X  => ');
+    x1 = parseInt(posX);
+
+    posY = await askAndRead('Introduce the position select of the piece in the Y  => ');
+    y1 = parseInt(posY);
+
+    const player1 = new Player(name, color, 12, game);
+    player1.selectPiece({ x: x1, y: y1 });
+
+    posX = await askAndRead('Introduce the NEW position for move the piece in  X  => ');
+    x1 = parseInt(posX);
+    posY = await askAndRead('Introduce the NEW position for move the piece in  Y  => ');
+    y1 = parseInt(posY);
+
+    player1.movePiece({ x: x1, y: y1 });
+    game.refreshGrid();
+    console.log(game.showBoard());
+};
+main();
 
 
