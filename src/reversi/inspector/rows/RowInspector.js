6586069e6af39bed9a2984123ba426eabe30598d
Percy Fernandez <Percy.Fernandez@jala.university>
2023-02-15 21:41:16 -0400

    row refactoring

--- src/reversi/inspector/rows/RowInspector.js
+++ src/reversi/inspector/rows/RowInspector.js
@@ -1,3 +1,4 @@
+import {getPiecesToFlip} from './getPiecesToFlip';
 export class RowInspector {
     #grid;
 
@@ -6,47 +7,21 @@ export class RowInspector {
     #y;
 
     #player;
+    #position;
 
     constructor(position, grid, player) {
-        // this.newToken = newToken;
         this.#grid = grid;
-        this.#x = position[0];
-        this.#y = position[1];
+        this.#position=position;
         this.#player = player;
         this.checkRow();
     }
 
     checkRow() {
-        var gridXmax = 7;
-        var y = this.#y;
-        var flip = [];
-        var checkPieces = [];//[0,0,0,0,1,2,2,1]
-        for (var index = y + 1; index < gridXmax; index++) {//find pieces in a row to the right
-            if ((this.#grid[this.#x][index]) == this.#player || (this.#grid[this.#x][index] == 0)) {
-                if (this.#grid[this.#x][index] == this.#player) {
-                    flip = checkPieces;
-                }
-                break;
-            } else {
-                var arr = [];
-                arr = [this.#x, index];
-                checkPieces.push(arr);
-            }
-        }
-
-        for (var keyf1 = y - 1; keyf1 > 0; keyf1--) {//find pieces in a row to the left
-            if ((this.#grid[this.#x][keyf1]) == this.#player || (this.#grid[this.#x][keyf1] == 0)) {
-                if (this.#grid[this.#x][keyf1] === this.#player) {
-                    flip = checkPieces;
-                }
-                break;
-            } else {
-                var arr1 = [];
-                arr1 = [this.#x, keyf1];
-                checkPieces.push(arr1);
-            }
-        }
-
-        return flip;
+        var checkPiecesToFlip = [];
+        var piecesToFlip=new getPiecesToFlip()
+        var piecesToflipToRigth=piecesToFlip.getPiecesToFlipRigth(this.#position,this.#grid,this.#player);
+        var piecesToflipToLeft=piecesToFlip.getPiecesToFlipLeft(this.#position,this.#grid,this.#player);
+        checkPiecesToFlip=checkPiecesToFlip.concat(piecesToflipToRigth,piecesToflipToLeft)
+        return checkPiecesToFlip;
     }
 }
---
+++ src/reversi/inspector/rows/getPiecesToFlip.js
@@ -0,0 +1,44 @@
+export class getPiecesToFlip {
+
+    getPiecesToFlipRigth(position, grid, player) {
+        var gridXmax = 7;
+        var y = position[1];
+        var x=position[0];
+        var flip = [];
+        var checkPieces = [];
+        for (var index = y + 1; index < gridXmax; index++) {
+            if ((grid[x][index]) == player || (grid[x][index] == 0)) {
+                if (grid[x][index] == player) {
+                    flip = checkPieces;
+                }
+                break;
+            } else {
+                var arr = [];
+                arr = [x, index];
+                checkPieces.push(arr);
+            }
+        }
+        return flip;
+    }
+
+    getPiecesToFlipLeft(position, grid, player) {
+       var y = position[1];
+        var x=position[0];
+        var flip = [];
+        var checkPieces = [];
+        for (var index = y - 1; index > 0; index--) {
+            if ((grid[x][index]) == player || (grid[x][index] == 0)) {
+                if (grid[x][index] == player) {
+                    flip = checkPieces;
+                }
+                break;
+            } else {
+                var arr = [];
+                arr = [x, index];
+                checkPieces.push(arr);
+            }
+        }
+        return flip;
+    }
+
+}
