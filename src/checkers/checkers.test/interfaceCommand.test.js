1e132e30097e35768edf9ff777cc5403f10eb290
Williams Velasquez <Williams.Velasquez@jala.university>
2023-02-21 17:09:35 -0400

    Add test case and order in folder test

--- src/checkers/checkers.test/interfaceCommand.test.js
+++ src/checkers/checkers.test/interfaceCommand.test.js
@@ -2,72 +2,72 @@ import { Command } from '../interfaceCommand';
 
 describe('Command', () => {
     let command;
-  
+
     beforeEach(() => {
-      command = new Command();
+        command = new Command();
     });
-  
+
     describe('isValidName', () => {
-      it('returns a valid name when a non-empty string is provided', async () => {
-        const name = await command.isValidName('John');
-        expect(name).toBe('John');
-      });
+        it('returns a valid name when a non-empty string is provided', async () => {
+            const name = await command.isValidName('John');
+            expect(name).toBe('John');
+        });
     });
-  
+
     describe('inputForSelectPiece', () => {
-      it('selects the piece at the given position', async () => {
-        const mockPlayer = {
-            movePiece: jest.fn(),
-            game: {
-                refreshGrid: jest.fn(),
-                showBoard: jest.fn(),
-            },
-        };
-        const command = new Command();
+        it('selects the piece at the given position', async () => {
+            const mockPlayer = {
+                movePiece: jest.fn(),
+                game: {
+                    refreshGrid: jest.fn(),
+                    showBoard: jest.fn(),
+                },
+            };
+            const command = new Command();
 
-        jest.spyOn(command, 'askAndRead')
-            .mockReturnValueOnce(Promise.resolve('1'))
-            .mockReturnValueOnce(Promise.resolve('2'));
+            jest.spyOn(command, 'askAndRead')
+                .mockReturnValueOnce(Promise.resolve('1'))
+                .mockReturnValueOnce(Promise.resolve('2'));
 
-        await command.inputForMovePiece(mockPlayer);
+            await command.inputForMovePiece(mockPlayer);
 
-        expect(mockPlayer.movePiece).toHaveBeenCalledWith({ x: 1, y: 2 });
-        expect(mockPlayer.game.refreshGrid).toHaveBeenCalled();
-        expect(mockPlayer.game.showBoard).toHaveBeenCalled();
-      });
+            expect(mockPlayer.movePiece).toHaveBeenCalledWith({ x: 1, y: 2 });
+            expect(mockPlayer.game.refreshGrid).toHaveBeenCalled();
+            expect(mockPlayer.game.showBoard).toHaveBeenCalled();
+        });
     });
-  
+
     describe('inputForMovePiece', () => {
-      it('moves the piece to the given position', async () => {
-        const mockAskAndRead = jest.spyOn(command, 'askAndRead')
-          .mockResolvedValueOnce('3')
-          .mockResolvedValueOnce('4');
-  
-        const player = { movePiece: jest.fn(), game: { refreshGrid: jest.fn(), showBoard: jest.fn() } };
-        await command.inputForMovePiece(player);
-  
-        expect(player.movePiece).toHaveBeenCalledWith({ x: 3, y: 4 });
-        expect(player.game.refreshGrid).toHaveBeenCalled();
-        expect(player.game.showBoard).toHaveBeenCalled();
-  
-        mockAskAndRead.mockRestore();
-      });
+        it('moves the piece to the given position', async () => {
+            const mockAskAndRead = jest.spyOn(command, 'askAndRead')
+                .mockResolvedValueOnce('3')
+                .mockResolvedValueOnce('4');
+
+            const player = { movePiece: jest.fn(), game: { refreshGrid: jest.fn(), showBoard: jest.fn() } };
+            await command.inputForMovePiece(player);
+
+            expect(player.movePiece).toHaveBeenCalledWith({ x: 3, y: 4 });
+            expect(player.game.refreshGrid).toHaveBeenCalled();
+            expect(player.game.showBoard).toHaveBeenCalled();
+
+            mockAskAndRead.mockRestore();
+        });
     });
-  
+
     describe('inputForKillPiece', () => {
-      it('kills the enemy piece and moves to the given position', async () => {
-        const mockAskAndRead = jest.spyOn(command, 'askAndRead')
-          .mockResolvedValueOnce('5')
-          .mockResolvedValueOnce('6');
-  
-        const player = { killEnemyPiece: jest.fn(), game: { refreshGrid: jest.fn(), showBoard: jest.fn() } };
-        await command.inputForKillPiece(player);
-  
-        expect(player.killEnemyPiece).toHaveBeenCalledWith({ x: 5, y: 6 });
-        expect(player.game.refreshGrid).toHaveBeenCalled();
-        expect(player.game.showBoard).toHaveBeenCalled();
-  
-        mockAskAndRead.mockRestore();
-      });
+        it('kills the enemy piece and moves to the given position', async () => {
+            const mockAskAndRead = jest.spyOn(command, 'askAndRead')
+                .mockResolvedValueOnce('5')
+                .mockResolvedValueOnce('6');
+
+            const player = { killEnemyPiece: jest.fn(), game: { refreshGrid: jest.fn(), showBoard: jest.fn() } };
+            await command.inputForKillPiece(player);
+
+            expect(player.killEnemyPiece).toHaveBeenCalledWith({ x: 5, y: 6 });
+            expect(player.game.refreshGrid).toHaveBeenCalled();
+            expect(player.game.showBoard).toHaveBeenCalled();
+
+            mockAskAndRead.mockRestore();
+        });
     });
-  });
+});
